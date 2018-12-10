31657141c89be768d26759dbc1d5da95d66f4074
JoseFerrer <jcfb90@gmail.com>
2018-12-10 18:29:33 -0500

    change => first commit

---
+++ src/_nav.js
@@ -0,0 +1,71 @@
+export default {
+  items: [
+    {
+      name: "Configuración",
+      url: "/base",
+      icon: "",
+      children: [
+        {
+          name: "Pais",
+          icon: "icon-settings"
+        },
+        {
+          name: "Empresa",
+          icon: "icon-settings"
+        },
+        {
+          name: "Ciudad",
+          icon: "icon-settings"
+        },
+        {
+          name: "Cargo",
+          icon: "icon-settings"
+        },
+        {
+          name: "Conglomerado",
+          icon: "icon-settings"
+        },
+        {
+          name: "Dependencia",
+          icon: "icon-settings"
+        },
+        {
+          name: "Departamento",
+          icon: "icon-settings"
+        },
+        {
+          name: "Remitente",
+          icon: "icon-settings"
+        },
+        {
+          name: "Usuarios",
+          icon: "icon-user"
+        },
+        {
+          name: "Sedes",
+          icon: "icon-settings"
+        },
+        {
+          name: "Cargo",
+          icon: "icon-settings"
+        },
+        {
+          name: "Roles y permisos",
+          icon: "icon-lock"
+        },
+        {
+          name: "Grupos de usuarios",
+          icon: "icon-people"
+        },
+        {
+          name: "Tipo documental",
+          icon: "icon-folder-alt"
+        },
+        {
+          name: "Tipo documental radicacion",
+          icon: "icon-book-open"
+        }
+      ]
+    }
+  ]
+};

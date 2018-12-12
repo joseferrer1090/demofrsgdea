e7ab1b84f63da255790b23eaee25529a16efde47
JoseFerrer <jcfb90@gmail.com>
2018-12-12 17:33:50 -0500

    Change => starting form create conglomerado

--- src/views/Conglomerado/components/FormCreateConglomerado.js
+++ src/views/Conglomerado/components/FormCreateConglomerado.js
@@ -10,15 +10,25 @@ class FormCreateConglomerado extends Component {
   render() {
     return (
       <div>
-        <Card className="border-success">
+        <Card className="">
           <CardHeader>Registro de Conglomerado</CardHeader>
           <CardBody>
-            <p>Fomulario de Registro con los datos</p>
+            <form className="form">
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <label>
+                      {" "}
+                      Dato1 <span className="text-danger">*</span>{" "}
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </form>
           </CardBody>
           <CardFooter>
             <Button className="btn btn-secundary pull-right">
-              {" "}
-              Registrar{" "}
+              <i className="fa fa-plus" /> Registrar{" "}
             </Button>
           </CardFooter>
         </Card>

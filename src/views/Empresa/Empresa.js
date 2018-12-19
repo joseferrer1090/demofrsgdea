0c92c0cd145ec5990a2233cee0e31db7a34aa741
JoseFerrer <jcfb90@gmail.com>
2018-12-19 16:24:57 -0500

    Change => views in other components

--- src/views/Empresa/Empresa.js
+++ src/views/Empresa/Empresa.js
@@ -16,12 +16,82 @@ import {
   CardFooter,
   CardHeader
 } from "reactstrap";
+import classnames from "classnames";
 
 class Empresa extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: "1"
+    };
+  }
+
+  toggle = tab => {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab
+      });
+    }
+  };
+
   render() {
     return (
       <div className="animated fadeIn">
-        <p> Probnado </p>
+        <Nav tabs>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === "1" })}
+              onClick={() => {
+                this.toggle("1");
+              }}
+            >
+              <i className="fa fa-plus" /> Registrar
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === "2" })}
+              onClick={() => {
+                this.toggle("2");
+              }}
+            >
+              <i className="fa fa-gear" /> Administrar empresas
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === "3" })}
+              onClick={() => {
+                this.toggle("3");
+              }}
+            >
+              <i className="fa fa-upload" /> Importar
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <TabContent activeTab={this.state.activeTab}>
+          <TabPane tabId="1">
+            <Row>
+              <Col md="12">
+                <p>Probando</p>
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="2">
+            <Row>
+              <Col md="12">
+                <p>Probando 2</p>
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="3">
+            <Row>
+              <Col md="12">
+                <p>Probando 3</p>
+              </Col>
+            </Row>
+          </TabPane>
+        </TabContent>
       </div>
     );
   }

29c488c7f7089ec7c742da5d1ba5fa37cf17765e
JoseFerrer <jcfb90@gmail.com>
2018-12-19 16:51:22 -0500

    Change => Adding components in the other views

--- src/views/Pais/Pais.js
+++ src/views/Pais/Pais.js
@@ -1,11 +1,90 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import {
+  Nav,
+  NavItem,
+  NavLink,
+  TabContent,
+  TabPane,
+  Row,
+  Col
+} from "reactstrap";
+import classnames from "classnames";
 
 class Pais extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: "1"
+    };
+  }
+
+  toggle = tab => {
+    if (this.state.activeTab !== 0) {
+      this.setState({
+        activeTab: tab
+      });
+    }
+  };
+
   render() {
     return (
       <div className="animated fadeIn">
-        <p>Probando</p>
+        <Nav tabs>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === "1" })}
+              onClick={() => {
+                this.toggle("1");
+              }}
+            >
+              <i className="fa fa-plus" /> Registrar
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === "2" })}
+              onClick={() => {
+                this.toggle("2");
+              }}
+            >
+              <i className="fa fa-gear" /> Administrar pais
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === "3" })}
+              onClick={() => {
+                this.toggle("3");
+              }}
+            >
+              <i className="fa fa-upload" /> Importar
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <TabContent activeTab={this.state.activeTab}>
+          <TabPane tabId="1">
+            <Row>
+              <Col md="12">
+                <p>Probando</p>
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="2">
+            <Row>
+              <Col md="12">
+                <p>Probando 2</p>
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="3">
+            <Row>
+              <Col md="12">
+                <p>Probando 3</p>
+              </Col>
+            </Row>
+          </TabPane>
+        </TabContent>
       </div>
     );
   }

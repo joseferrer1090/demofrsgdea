dc0b1138cb886d4af49ab1a92d2f977f154d8589
joseferrer1090 <jcfb90@gmail.com>
2019-01-02 14:47:42 -0500

    Change => Adding modal for view Remitente

---
+++ src/views/Remitentes/components/ModalViewRemitente.js
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Modal, ModalFooter, ModalHeader, ModalBody } from "reactstrap";
+
+class ModalViewRemitente extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: this.props.modalview
+    };
+  }
+
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Modal isOpen={this.state.modal}>
+          <ModalHeader>Remitente</ModalHeader>
+          <ModalBody>
+            <div className="table-responsive">
+              <table className="table table-hover table-striped">
+                <tbody>
+                  <tr>
+                    <td> Identificación: </td>
+                    <td> </td>
+                  </tr>
+                  <tr>
+                    <td> Nombre: </td>
+                    <td> </td>
+                  </tr>
+                  <tr>
+                    <td> Email: </td>
+                    <td> </td>
+                  </tr>
+                  <tr>
+                    <td> Teléfono: </td>
+                    <td> </td>
+                  </tr>
+                  <tr>
+                    <td> Dirección: </td>
+                    <td> </td>
+                  </tr>
+                  <tr>
+                    <td> Ciudad: </td>
+                    <td> </td>
+                  </tr>
+                  <tr>
+                    <td> Estado: </td>
+                    <td> </td>
+                  </tr>
+                  <tr>
+                    <td> Observación: </td>
+                    <td> </td>
+                  </tr>
+                  <tr>
+                    <td> Fecha de Creación: </td>
+                    <td> </td>
+                  </tr>
+                  <tr>
+                    <td> Fecha de Modificación: </td>
+                    <td> </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <button type="button" className="btn btn-secondary">
+              {" "}
+              <i
+                className="fa fa-times"
+                onClick={() => {
+                  this.setState({ modal: false });
+                }}
+              />{" "}
+              Cerrar{" "}
+            </button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+ModalViewRemitente.propTypes = {
+  modalview: PropTypes.bool.isRequired
+};
+
+export default ModalViewRemitente;
--- src/views/Remitentes/components/TableContentRemitente.js
+++ src/views/Remitentes/components/TableContentRemitente.js
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
 import { Row, Col } from "reactstrap";
+import ModalView from "./ModalViewRemitente";
 
 import "./../../../../node_modules/react-bootstrap-table/css/react-bootstrap-table.css";
+import ModalViewRemitente from "./ModalViewRemitente";
 
 const dataExample = [
   {
@@ -39,8 +41,29 @@ const dataExample = [
 class TableContentRemitente extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      modalViewRemitente: false
+    };
   }
+
+  accionesRemitente(cel, row) {
+    return (
+      <button
+        className="btn btn-secondary"
+        onClick={() => {
+          this.openModalView();
+        }}
+      >
+        {" "}
+        <i className="fa fa-eye" />{" "}
+      </button>
+    );
+  }
+
+  openModalView() {
+    this.refs.child.toggle();
+  }
+
   render() {
     return (
       <div className="animated fadeIn">
@@ -53,26 +76,43 @@ class TableContentRemitente extends Component {
               searchPlaceholder="Buscar"
               exportCSV
             >
-              <TableHeaderColumn isKey dataField={"id"}>
+              <TableHeaderColumn
+                isKey
+                dataField={"id"}
+                dataAlign="center"
+                width="50"
+              >
                 #
               </TableHeaderColumn>
-              <TableHeaderColumn dataField={"identificacion"}>
+              <TableHeaderColumn
+                dataField={"identificacion"}
+                dataAlign="center"
+              >
                 {" "}
                 identificación{" "}
               </TableHeaderColumn>
-              <TableHeaderColumn dataField={"nombre"}>
+              <TableHeaderColumn dataField={"nombre"} dataAlign="center">
                 {" "}
                 Nombre{" "}
               </TableHeaderColumn>
-              <TableHeaderColumn dataField={"email"}> Email </TableHeaderColumn>
-              <TableHeaderColumn dataField={"estado"}>
+              <TableHeaderColumn dataField={"email"} dataAlign="center">
+                {" "}
+                Email{" "}
+              </TableHeaderColumn>
+              <TableHeaderColumn dataField={"estado"} dataAlign="center">
                 {" "}
                 Estado{" "}
               </TableHeaderColumn>
-              <TableHeaderColumn> Acciones </TableHeaderColumn>
+              <TableHeaderColumn
+                export={false}
+                dataAlign="center"
+                dataFormat={(cell, row) => this.accionesRemitente(cell, row)}
+                style={{ border: "none" }}
+              />
             </BootstrapTable>
           </Col>
         </Row>
+        <ModalView modalview={this.state.modalViewRemitente} ref="child" />
       </div>
     );
   }

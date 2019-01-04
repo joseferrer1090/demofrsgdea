ffba0d2039efd53f6a0953a23325297212afbe75
JoseFerrer <jcfb90@gmail.com>
2019-01-04 18:33:35 -0500

    Change => starting components for auditoria

--- src/views/Auditoria/Auditoria.js
+++ src/views/Auditoria/Auditoria.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Row, Col } from "reactstrap";
 
 const Auditoria = () => {
   return (
-    <div>
-      <p> Probando este componente </p>
+    <div className="animated fadeIn">
+      <Row>
+        <Col sm="6" md={{ offset: 3 }}>
+          <div className="card" style={{ border: "1px solid black" }}>
+            <div className="card-header">Consulta Auditoria</div>
+            <div className="card-body">
+              <p> Probando </p>
+            </div>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };

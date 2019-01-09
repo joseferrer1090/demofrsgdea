737ec70244e04c8f5cf865d2a76a7a8b2883199e
joseferrer1090 <jcfb90@gmail.com>
2019-01-09 17:24:07 -0500

    Change => resolve error in the view remitente

--- src/views/Remitentes/components/FormCreateRemitente.js
+++ src/views/Remitentes/components/FormCreateRemitente.js
@@ -55,157 +55,156 @@ class FormCreateRemitente extends Component {
 
     return (
       <div className="animated fadeIn">
-        <Row>
-          <Col sm="8" md={{ offset: 2 }}>
-            <Card>
-              <CardHeader> Registro de remitente </CardHeader>
-              <CardBody>
-                <form className="form">
-                  <div className="row">
-                    <div className="col-md-6">
-                      <div className="form-group">
-                        <label>
-                          {" "}
-                          Identificaicón <span className="text-danger">
-                            *
-                          </span>{" "}
-                        </label>
-                        <input type="text" className="form-control" />
-                      </div>
-                    </div>
-                    <div className="col-md-6">
-                      <div className="form-group">
-                        <label>
-                          {" "}
-                          Nombre <span className="text-danger">*</span>{" "}
-                        </label>
-                        <input className="form-control" type="text" />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-md-4">
-                      <div className="form-group">
-                        <label>
-                          {" "}
-                          Email <span className="text-danger">*</span>{" "}
-                        </label>
-                        <input type="text" className="form-control" />
-                      </div>
-                    </div>
-                    <div className="col-md-4">
-                      <div className="form-group">
-                        <label> Teléfono </label>
-                        <input type="text" className="form-control" />
-                      </div>
-                    </div>
-                    <div className="col-md-4">
-                      <div className="form-group">
-                        <label> Dirección </label>
-                        <input type="text" className="form-control" />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-md-4">
-                      <div className="form-group">
-                        <label>
-                          {" "}
-                          País <span className="text-danger"> * </span>{" "}
-                        </label>
-                        <Select
-                          value={SelectedOptionPais}
-                          onChange={this.handleChangeSelectedPais}
-                          options={dataExamplePais}
-                        />
-                      </div>
-                    </div>
-                    <div className="col-md-4">
-                      <div className="form-group">
-                        <label>
-                          {" "}
-                          Departamento <span className="text-danger">
+        <div className="container">
+          <Row>
+            <Col sm="8" md={{ offset: 2 }}>
+              <Card>
+                <CardHeader> Registro de remitente </CardHeader>
+                <CardBody>
+                  <form className="form">
+                    <div className="row">
+                      <div className="col-md-6">
+                        <div className="form-group">
+                          <label>
                             {" "}
-                            *{" "}
-                          </span>{" "}
-                        </label>
-                        <Select
-                          value={SelectedOptionDepartamento}
-                          onChange={this.handleChangeSelectedDepartamento}
-                          options={dataExampleDepartamento}
-                        />
-                      </div>
-                    </div>
-                    <div className="col-md-4">
-                      <div className="form-group">
-                        <label>
-                          {" "}
-                          Ciudad <span className="text-danger"> * </span>{" "}
-                        </label>
-                        <Select
-                          value={SelectedOptionCiudades}
-                          onChange={this.handleChangeSelected}
-                          options={dataExample}
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-md-12">
-                      <div className="form-group">
-                        <label> Observación </label>
-                        <textarea className="form-control" />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-md-12">
-                      <div className="form-group">
-                        <label>
-                          {" "}
-                          Estado <span className="text-danger">*</span>{" "}
-                        </label>
-                        <div className="form-check">
-                          <input
-                            type="checkbox"
-                            className="form-check-input"
-                            id="exampleCheck1"
-                          />
-                          <label
-                            className="form-check-label"
-                            htmlFor="exampleCheck1"
-                          >
-                            Activar
+                            Identificaicón{" "}
+                            <span className="text-danger">*</span>{" "}
                           </label>
-                          <p
-                            className="text-muted"
-                            style={{ textAlign: "justify" }}
-                          >
-                            Si esta opción se encuentra activada, representa que
-                            el remitente es visible en el sistema y se podrán
-                            realizar operaciones entre cada uno de los módulos
-                            correspondientes de la aplicación. En caso contrario
-                            el remitente no se elimina del sistema solo quedará
-                            inactivo e invisibles para cada uno de los módulos
-                            correspondiente del sistema.
-                          </p>
+                          <input type="text" className="form-control" />
+                        </div>
+                      </div>
+                      <div className="col-md-6">
+                        <div className="form-group">
+                          <label>
+                            {" "}
+                            Nombre <span className="text-danger">*</span>{" "}
+                          </label>
+                          <input className="form-control" type="text" />
                         </div>
                       </div>
                     </div>
+                    <div className="row">
+                      <div className="col-md-4">
+                        <div className="form-group">
+                          <label>
+                            {" "}
+                            Email <span className="text-danger">*</span>{" "}
+                          </label>
+                          <input type="text" className="form-control" />
+                        </div>
+                      </div>
+                      <div className="col-md-4">
+                        <div className="form-group">
+                          <label> Teléfono </label>
+                          <input type="text" className="form-control" />
+                        </div>
+                      </div>
+                      <div className="col-md-4">
+                        <div className="form-group">
+                          <label> Dirección </label>
+                          <input type="text" className="form-control" />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-md-4">
+                        <div className="form-group">
+                          <label>
+                            {" "}
+                            País <span className="text-danger"> * </span>{" "}
+                          </label>
+                          <Select
+                            value={SelectedOptionPais}
+                            onChange={this.handleChangeSelectedPais}
+                            options={dataExamplePais}
+                          />
+                        </div>
+                      </div>
+                      <div className="col-md-4">
+                        <div className="form-group">
+                          <label>
+                            {" "}
+                            Departamento{" "}
+                            <span className="text-danger"> * </span>{" "}
+                          </label>
+                          <Select
+                            value={SelectedOptionDepartamento}
+                            onChange={this.handleChangeSelectedDepartamento}
+                            options={dataExampleDepartamento}
+                          />
+                        </div>
+                      </div>
+                      <div className="col-md-4">
+                        <div className="form-group">
+                          <label>
+                            {" "}
+                            Ciudad <span className="text-danger"> * </span>{" "}
+                          </label>
+                          <Select
+                            value={SelectedOptionCiudades}
+                            onChange={this.handleChangeSelected}
+                            options={dataExample}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-md-12">
+                        <div className="form-group">
+                          <label> Observación </label>
+                          <textarea className="form-control" />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-md-12">
+                        <div className="form-group">
+                          <label>
+                            {" "}
+                            Estado <span className="text-danger">*</span>{" "}
+                          </label>
+                          <div className="form-check">
+                            <input
+                              type="checkbox"
+                              className="form-check-input"
+                              id="exampleCheck1"
+                            />
+                            <label
+                              className="form-check-label"
+                              htmlFor="exampleCheck1"
+                            >
+                              Activar
+                            </label>
+                            <p
+                              className="text-muted"
+                              style={{ textAlign: "justify" }}
+                            >
+                              Si esta opción se encuentra activada, representa
+                              que el remitente es visible en el sistema y se
+                              podrán realizar operaciones entre cada uno de los
+                              módulos correspondientes de la aplicación. En caso
+                              contrario el remitente no se elimina del sistema
+                              solo quedará inactivo e invisibles para cada uno
+                              de los módulos correspondiente del sistema.
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                </CardBody>
+                <CardFooter>
+                  <div className="float-right">
+                    <button className="btn btn-secondary">
+                      {" "}
+                      <i className="fa fa-plus" /> Registrar{" "}
+                    </button>
                   </div>
-                </form>
-              </CardBody>
-              <CardFooter>
-                <div className="float-right">
-                  <button className="btn btn-secondary">
-                    {" "}
-                    <i className="fa fa-plus" /> Registrar{" "}
-                  </button>
-                </div>
-              </CardFooter>
-            </Card>
-          </Col>
-        </Row>
+                </CardFooter>
+              </Card>
+            </Col>
+          </Row>
+        </div>
       </div>
     );
   }

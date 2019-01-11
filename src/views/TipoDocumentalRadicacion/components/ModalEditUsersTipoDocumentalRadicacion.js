8dc1714fe295002e959cb8f504876cabff5f74b3
joseferrer1090 <jcfb90@gmail.com>
2019-01-11 16:04:41 -0500

    Change => adding component user for modal edit user on view tipo documental radicacion

--- src/views/TipoDocumentalRadicacion/components/ModalEditUsersTipoDocumentalRadicacion.js
+++ src/views/TipoDocumentalRadicacion/components/ModalEditUsersTipoDocumentalRadicacion.js
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Row,
+  Col,
+  Card,
+  CardFooter,
+  CardBody
+} from "reactstrap";
 
 class ModalEditUsersTipoDocumentalRadicacion extends Component {
   constructor(props) {
@@ -18,12 +28,93 @@ class ModalEditUsersTipoDocumentalRadicacion extends Component {
 
   render() {
     return (
-      <Modal isOpen={this.state.modal}>
+      <Modal className="modal-lg" isOpen={this.state.modal}>
         <ModalHeader> Actualizar asignación de usuarios </ModalHeader>
         <ModalBody>
-          <p> Probando </p>
+          <div className="row">
+            <div className="col-md-12">
+              <Card>
+                <CardBody>
+                  <h4 className=""> Búsqueda de usuarios </h4>
+                  <hr />
+                  <br />
+                  <form className="form">
+                    <div className="row">
+                      <div className="col-md-4">
+                        <div className="form-group">
+                          <label>
+                            {" "}
+                            Empresa <span className="text-danger">*</span>{" "}
+                          </label>
+                          <select className="form-control">
+                            {" "}
+                            <option> Seleccione </option>{" "}
+                          </select>
+                        </div>
+                      </div>
+                      <div className="col-md-4">
+                        <div className="form-group">
+                          <label>
+                            {" "}
+                            Sede <span className="text-danger">*</span>{" "}
+                          </label>
+                          <select className="form-control">
+                            {" "}
+                            <option> Seleccione </option>{" "}
+                          </select>
+                        </div>
+                      </div>
+                      <div className="col-md-4">
+                        <div className="form-group">
+                          <label>
+                            {" "}
+                            Dependencia <span className="text-danger">
+                              *
+                            </span>{" "}
+                          </label>
+                          <select className="form-control">
+                            {" "}
+                            <option> Seleccione </option>{" "}
+                          </select>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <label> Usuarios disponibles </label>
+                      <textarea className="form-control" disabled />
+                    </div>
+                  </form>
+                </CardBody>
+                <CardFooter>
+                  <div className="float-right">
+                    <button className="btn btn-secondary btn-sm">
+                      {" "}
+                      <i className="fa fa-search" /> Buscar
+                    </button>{" "}
+                  </div>
+                </CardFooter>
+              </Card>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <div className="form-group">
+                <label>
+                  {" "}
+                  Usuarios asignados <span className="text-danger">*</span>{" "}
+                </label>
+                <select className="form-control">
+                  <option className=""> seleccione </option>
+                </select>
+              </div>
+            </div>
+          </div>
         </ModalBody>
         <ModalFooter>
+          <button type="button" className="btn btn-outline-success">
+            {" "}
+            <i className="fa fa-pencil" /> Actualizar usuarios{" "}
+          </button>
           <button
             className="btn btn-secondary"
             onClick={() => {

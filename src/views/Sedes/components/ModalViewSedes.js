1392efae83414bab4072c8bd19477e81472c720f
joseferrer1090 <jcfb90@gmail.com>
2019-01-16 15:17:22 -0500

    Change => Adding component modal in the view sedes

--- src/views/Sedes/components/ModalViewSedes.js
+++ src/views/Sedes/components/ModalViewSedes.js
@@ -22,19 +22,93 @@ class ModalViewSedes extends Component {
         <Modal className="modal-lg" isOpen={this.state.modal}>
           <ModalHeader> Ver sede </ModalHeader>
           <ModalBody>
-            <div className="table-responsive">
-              <table className="table table-striped">
-                <tbody>
-                  <tr>
-                    <td> Código: </td>
-                    <td> Dato </td>
-                  </tr>
-                  <tr>
-                    <td> Empresa: </td>
-                    <td> Dato </td>
-                  </tr>
-                </tbody>
-              </table>
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12">
+                  <table className="table table-striped table-hover">
+                    <tbody>
+                      <tr>
+                        <td>
+                          <dl className="param">
+                            <dt>Código: </dt>
+                            <dd> THE WORLD BANK</dd>
+                          </dl>
+                        </td>
+                        <td>
+                          <dl className="param">
+                            <dt>Empresa: </dt>
+                            <dd> 12345678912345</dd>
+                          </dl>
+                        </td>
+                        <td>
+                          <dl className="param">
+                            <dt>Nombre: </dt>
+                            <dd> 123456789</dd>
+                          </dl>
+                        </td>
+                        <td>
+                          <dl className="param">
+                            <dt>Fecha de creación: </dt>
+                            <dd> dd/mm/aaaa</dd>
+                          </dl>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <dl className="param">
+                            <dt>Prefijo de radicación: </dt>
+                            <dd> THE WORLD BANK</dd>
+                          </dl>
+                        </td>
+                        <td>
+                          <dl className="param">
+                            <dt>Dirección: </dt>
+                            <dd> 12345678912345</dd>
+                          </dl>
+                        </td>
+                        <td>
+                          <dl className="param">
+                            <dt>Teléfono: </dt>
+                            <dd> 123456789</dd>
+                          </dl>
+                        </td>
+                        <td>
+                          <dl className="param">
+                            <dt>Fecha de modificación: </dt>
+                            <dd> dd/mm/aaaa</dd>
+                          </dl>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <dl className="param">
+                            <dt>Secuencia de radicación: </dt>
+                            <dd> THE WORLD BANK</dd>
+                          </dl>
+                        </td>
+                        <td>
+                          <dl className="param">
+                            <dt>Ciudad: </dt>
+                            <dd> 12345678912345</dd>
+                          </dl>
+                        </td>
+                        <td>
+                          <dl className="param">
+                            <dt>Rol responsable: </dt>
+                            <dd> 123456789</dd>
+                          </dl>
+                        </td>
+                        <td>
+                          <dl className="param">
+                            <dt>Estado: </dt>
+                            <dd> activo / inactivo </dd>
+                          </dl>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
             </div>
           </ModalBody>
           <ModalFooter>

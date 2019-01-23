49ce385902d27f3e139d1370e9402544db74e439
joseferrer1090 <jcfb90@gmail.com>
2019-01-23 10:30:00 -0500

    Change => optimize modal view remitente

--- src/views/Remitentes/components/ModalViewRemitente.js
+++ src/views/Remitentes/components/ModalViewRemitente.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Modal, ModalFooter, ModalHeader, ModalBody, Button } from "reactstrap";
+import {
+  Modal,
+  ModalFooter,
+  ModalHeader,
+  ModalBody,
+  Button,
+  Row,
+  Col,
+  Collapse,
+  Card,
+  CardHeader,
+  CardBody
+} from "reactstrap";
+import IMGPROFILE from "./../../../assets/img/profile.svg";
 
 class ModalViewRemitente extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      modal: this.props.modalview
+      modal: this.props.modalview,
+      collapse: false
     };
   }
 
@@ -16,58 +30,126 @@ class ModalViewRemitente extends Component {
     });
   };
 
+  toggleCollapse = () => {
+    this.setState({ collapse: !this.state.collapse });
+  };
+
   render() {
     return (
       <div>
-        <Modal isOpen={this.state.modal}>
+        <Modal className="modal-lg" isOpen={this.state.modal}>
           <ModalHeader>Ver remitente</ModalHeader>
           <ModalBody>
-            <div className="table-responsive">
-              <table className="table table-hover table-striped">
-                <tbody>
-                  <tr>
-                    <td> Identificación: </td>
-                    <td> </td>
-                  </tr>
-                  <tr>
-                    <td> Nombre: </td>
-                    <td> </td>
-                  </tr>
-                  <tr>
-                    <td> Email: </td>
-                    <td> </td>
-                  </tr>
-                  <tr>
-                    <td> Teléfono: </td>
-                    <td> </td>
-                  </tr>
-                  <tr>
-                    <td> Dirección: </td>
-                    <td> </td>
-                  </tr>
-                  <tr>
-                    <td> Ciudad: </td>
-                    <td> </td>
-                  </tr>
-                  <tr>
-                    <td> Estado: </td>
-                    <td> </td>
-                  </tr>
-                  <tr>
-                    <td> Observación: </td>
-                    <td> </td>
-                  </tr>
-                  <tr>
-                    <td> Fecha de Creación: </td>
-                    <td> </td>
-                  </tr>
-                  <tr>
-                    <td> Fecha de Modificación: </td>
-                    <td> </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+            <Row>
+              <Col sm="3">
+                <img src={IMGPROFILE} className="img-thumbnail" />
+              </Col>
+              <Col sm="9">
+                <div className="">
+                  {" "}
+                  <h5 className="" style={{ borderBottom: "1px solid black" }}>
+                    {" "}
+                    Datos personales{" "}
+                  </h5>{" "}
+                </div>
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <dl className="param">
+                        <dt>Identificación </dt>
+                        <dd> 1047425246 </dd>
+                      </dl>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <dl className="param">
+                        <dt>Nombre </dt>
+                        <dd> Jose Carlos Ferrer Bermudez</dd>
+                      </dl>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <dl className="param">
+                        <dt>E-mail </dt>
+                        <dd> jcfb90@gmail.com </dd>
+                      </dl>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <dl className="param">
+                        <dt>Teléfono </dt>
+                        <dd> 301-7923-466 </dd>
+                      </dl>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <dl className="param">
+                        <dt>Dirección </dt>
+                        <dd> Cra 44c # 22 - 86 int 702</dd>
+                      </dl>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <dl className="param">
+                        <dt>Ciudad </dt>
+                        <dd>San petesburgo</dd>
+                      </dl>
+                    </div>
+                  </div>
+                </div>
+              </Col>
+              <Col sm="12">
+                <Card>
+                  <CardHeader>
+                    {" "}
+                    <a onClick={this.toggleCollapse}> + información </a>{" "}
+                  </CardHeader>
+                  <Collapse isOpen={this.state.collapse}>
+                    <CardBody>
+                      <div className="row">
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <dl className="param">
+                              <dt> Estado </dt>
+                              <dd> estado </dd>
+                            </dl>
+                          </div>
+                        </div>
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <dl className="param">
+                              <dt>Observación </dt>
+                              <dd> observación </dd>
+                            </dl>
+                          </div>
+                        </div>
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <dl className="param">
+                              <dt>Fecha de creación </dt>
+                              <dd> fecha de creación </dd>
+                            </dl>
+                          </div>
+                        </div>
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <dl className="param">
+                              <dt>Fecha de modificación </dt>
+                              <dd> fecha de modificación </dd>
+                            </dl>
+                          </div>
+                        </div>
+                      </div>
+                    </CardBody>
+                  </Collapse>
+                </Card>
+              </Col>
+            </Row>
           </ModalBody>
           <ModalFooter>
             <button
@@ -77,8 +159,7 @@ class ModalViewRemitente extends Component {
                 this.setState({ modal: false });
               }}
             >
-              <i className="fa fa-times" />
-              Cerrar
+              <i className="fa fa-times" /> Cerrar
             </button>
           </ModalFooter>
         </Modal>

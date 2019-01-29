c437ed734103f6fff115080c7d877cc20544b605
joseferrer1090 <jcfb90@gmail.com>
2019-01-29 11:17:30 -0500

    Change => new modal view grupos

--- src/views/Grupos/components/ModalViewGrupo.js
+++ src/views/Grupos/components/ModalViewGrupo.js
@@ -6,13 +6,19 @@ import {
   ModalBody,
   ModalHeader,
   Row,
-  Col
+  Col,
+  Collapse,
+  Card,
+  CardHeader,
+  CardBody
 } from "reactstrap";
+
+import IMGGROUPOS from "./../../../assets/img/multiple-users-silhouette.svg";
 
 class ModalViewPais extends Component {
   constructor(props) {
     super(props);
-    this.state = { modal: this.props.modalview };
+    this.state = { modal: this.props.modalview, collapse: false };
   }
 
   toggle = () => {
@@ -21,50 +27,119 @@ class ModalViewPais extends Component {
     });
   };
 
+  toggleCollapse = () => {
+    this.setState({
+      collapse: !this.state.collapse
+    });
+  };
+
   render() {
     return (
       <div>
-        <Modal isOpen={this.state.modal}>
+        <Modal className="modal-lg" isOpen={this.state.modal}>
           <ModalHeader> Ver grupo </ModalHeader>
           <ModalBody>
             <Row>
-              <Col sm="12">
-                <form className="form">
-                  <div className="table-responsive">
-                    <table className="table table-hover table-striped">
-                      <tbody>
-                        <tr>
-                          <td> Código: </td>
-                          <td> </td>
-                        </tr>
-                        <tr>
-                          <td> Nombre: </td>
-                          <td> </td>
-                        </tr>
-                        <tr>
-                          <td> Descripción </td>
-                          <td> </td>
-                        </tr>
-                        <tr>
-                          <td> Estado: </td>
-                          <td> </td>
-                        </tr>
-                        <tr>
-                          <td> Usuarios </td>
-                          <td> </td>
-                        </tr>
-                        <tr>
-                          <td> Fecha de creación: </td>
-                          <td> </td>
-                        </tr>
-                        <tr>
-                          <td> Fecha de modificación: </td>
-                          <td> </td>
-                        </tr>
-                      </tbody>
-                    </table>
+              <Col sm="3">
+                <img src={IMGGROUPOS} className="img-thumbnail" width="170" />
+              </Col>
+              <Col sm="9">
+                <div className="">
+                  {" "}
+                  <h5 className="" style={{ borderBottom: "1px solid black" }}>
+                    {" "}
+                    Datos{" "}
+                  </h5>{" "}
+                </div>
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <dl className="param">
+                        <dt> Código </dt>
+                        <dd> codigo </dd>
+                      </dl>
+                    </div>
                   </div>
-                </form>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <dl className="param">
+                        <dt> Nombre </dt>
+                        <dd> nombre </dd>
+                      </dl>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <dl className="param">
+                        <dt> Descripción </dt>
+                        <dd> descripción </dd>
+                      </dl>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <dl className="param">
+                        <dt> Estado </dt>
+                        <dd> estado </dd>
+                      </dl>
+                    </div>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+            <br />
+            <Row>
+              <Col sm="12">
+                <Card>
+                  <CardHeader>
+                    {" "}
+                    <a
+                      onClick={() => {
+                        this.toggleCollapse();
+                      }}
+                      style={{ cursor: "pointer" }}
+                    >
+                      {" "}
+                      Más informacion{" "}
+                    </a>{" "}
+                  </CardHeader>
+                  <Collapse isOpen={this.state.collapse}>
+                    <CardBody>
+                      <div className="row">
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <dl className="param">
+                              <dt> Fecha de creaciónn </dt>
+                              <dd> fecha de creaciónn </dd>
+                            </dl>
+                          </div>
+                        </div>
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <dl className="param">
+                              <dt> Fecha de modificación </dt>
+                              <dd> fecha de modificación </dd>
+                            </dl>
+                          </div>
+                        </div>
+                        <div className="col-md-12">
+                          <div className="form-group">
+                            <dl className="param">
+                              <dt> Usuarios </dt>
+                              <dd>
+                                {" "}
+                                <textarea
+                                  className="form-control"
+                                  disabled
+                                />{" "}
+                              </dd>
+                            </dl>
+                          </div>
+                        </div>
+                      </div>
+                    </CardBody>
+                  </Collapse>
+                </Card>
               </Col>
             </Row>
           </ModalBody>

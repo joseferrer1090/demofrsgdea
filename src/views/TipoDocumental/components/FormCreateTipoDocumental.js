80a47b275decffb317d8e24156ff9bce9058dca3
joseferrer1090 <jcfb90@gmail.com>
2019-02-14 15:47:35 -0500

    Change => Adding new structure for component CreateFormTipoDocumentaL

--- src/views/TipoDocumental/components/FormCreateTipoDocumental.js
+++ src/views/TipoDocumental/components/FormCreateTipoDocumental.js
@@ -75,148 +75,149 @@ class FormCreateTipoDocumental extends Component {
   render() {
     return (
       <div className="animated fadeIn">
-        <div className="container">
-          <Row>
-            <Col sm="8" md={{ offset: 2 }}>
-              <Card>
-                <CardHeader> Registro tipo documental </CardHeader>
-                <CardBody>
-                  <Row>
-                    <Col sm="6">
-                      <div className="form-group">
-                        <label>
-                          {" "}
-                          Código <span className="text-danger">*</span>{" "}
-                        </label>
-                        <input type="text" className="form-control" />
-                      </div>
-                    </Col>
-                    <Col sm="6">
-                      <div className="form-group">
-                        <label>
-                          {" "}
-                          Nombre <span className="text-danger">*</span>{" "}
-                        </label>
-                        <input type="text" className="form-control" />
-                      </div>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col sm="6">
-                      <div className="form-group">
-                        <label>
-                          {" "}
-                          Dependencia <span className="text-danger">
-                            *
-                          </span>{" "}
-                        </label>
-                        <select className="form-control">
-                          <option> Seleccione </option>
-                        </select>
-                      </div>
-                    </Col>
-                    <Col sm="6">
-                      <div className="form-group">
-                        <label> Hora de respuesta </label>
-                        <input type="text" className="form-control" />
-                      </div>
-                    </Col>
-                    <Col sm="12">
-                      <div className="form-group">
-                        <label>
-                          {" "}
-                          Descripción <span className="text-danger">
-                            *
-                          </span>{" "}
-                        </label>
-                        <textarea className="form-control" />
-                      </div>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col sm="12">
-                      <Card>
-                        <CardBody>
-                          <CardTitle>
-                            {" "}
-                            Indices documentales <hr />{" "}
-                          </CardTitle>
-                          <div className="">
-                            <Card body>
-                              <div className="row">
-                                <div className="col-md-12">
-                                  <div className="text-center">
-                                    <p>
-                                      {" "}
-                                      A continuacion se deben agregar los
-                                      indices documentales del tipo documental{" "}
-                                    </p>
-                                    <button
-                                      className="btn btn-secondary"
-                                      onClick={this.addClick.bind(this)}
-                                      style={{ width: "150px" }}
-                                    >
-                                      {" "}
-                                      Agregar
-                                    </button>
-                                  </div>
-                                  <hr />
-                                  {this.createUI()}
-                                </div>
-                              </div>
-                            </Card>
+        <Row>
+          <Col sm="12">
+            <Card>
+              <CardBody>
+                <div className="row">
+                  <Col sm="6">
+                    <Card>
+                      <CardHeader> Datos del tipo documental </CardHeader>
+                      <CardBody>
+                        <Row>
+                          <Col sm="6">
+                            <div className="form-group">
+                              <label>
+                                {" "}
+                                Código <span className="text-danger">
+                                  *
+                                </span>{" "}
+                              </label>
+                              <input type="text" className="form-control" />
+                            </div>
+                          </Col>
+                          <Col sm="6">
+                            <div className="form-group">
+                              <label>
+                                {" "}
+                                Nombre <span className="text-danger">
+                                  *
+                                </span>{" "}
+                              </label>
+                              <input type="text" className="form-control" />
+                            </div>
+                          </Col>
+                          <Col sm="6">
+                            <div className="form-group">
+                              <label>
+                                {" "}
+                                Dependencia{" "}
+                                <span className="text-danger">*</span>{" "}
+                              </label>
+                              <select className="form-control">
+                                <option> Seleccione </option>
+                              </select>
+                            </div>
+                          </Col>
+                          <Col sm="6">
+                            <div className="form-group">
+                              <label> Hora de respuesta </label>
+                              <input type="text" className="form-control" />
+                            </div>
+                          </Col>
+                          <Col sm="12">
+                            <div className="form-group">
+                              <label>
+                                {" "}
+                                Descripción{" "}
+                                <span className="text-danger">*</span>{" "}
+                              </label>
+                              <textarea className="form-control" />
+                            </div>
+                          </Col>
+                        </Row>
+                      </CardBody>
+                    </Card>
+                  </Col>
+                  <Col sm="6">
+                    <Card>
+                      <CardHeader> Indices documentales </CardHeader>
+                      <CardBody>
+                        <div className="row">
+                          <div className="col-md-12">
+                            <div className="text-center">
+                              <p>
+                                {" "}
+                                A continuacion se deben agregar los indices
+                                documentales del tipo documental{" "}
+                              </p>
+                              <button
+                                className="btn btn-secondary"
+                                onClick={this.addClick.bind(this)}
+                                style={{ width: "150px" }}
+                              >
+                                {" "}
+                                Agregar
+                              </button>
+                            </div>
+                            <hr />
+                            {this.createUI()}
                           </div>
-                        </CardBody>
-                      </Card>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col sm="12">
-                      <div className="form-group">
-                        <label>
-                          {" "}
-                          Estado <span className="text-danger">*</span>{" "}
-                        </label>
-                        <div className="form-check">
-                          <input
-                            type="checkbox"
-                            className="form-check-input"
-                            id="exampleCheck1"
-                          />
-                          <label
-                            className="form-check-label"
-                            htmlFor="exampleCheck1"
-                          >
-                            Activar
-                          </label>
-                          <p
-                            className="text-muted"
-                            style={{ textAlign: "justify" }}
-                          >
-                            Si esta opción se encuentra activada, representa que
-                            el Tipo documental es visible en el sistema y se
-                            podrán realizar operaciones entre cada uno de los
-                            módulos correspondientes de la aplicación. En caso
-                            contrario el Tipo documental no se elimina del
-                            sistema solo quedará inactivo e invisibles para cada
-                            uno de los módulos correspondiente del sistema.
-                          </p>
                         </div>
+                      </CardBody>
+                    </Card>
+                  </Col>
+                  <Col sm="12">
+                    <div className="form-group">
+                      <label>
+                        {" "}
+                        Estado <span className="text-danger">*</span>{" "}
+                      </label>
+                      <div className="form-check">
+                        <input
+                          type="checkbox"
+                          className="form-check-input"
+                          id="exampleCheck1"
+                        />
+                        <label
+                          className="form-check-label"
+                          htmlFor="exampleCheck1"
+                        >
+                          Activar
+                        </label>
+                        <p
+                          className="text-muted"
+                          style={{ textAlign: "justify" }}
+                        >
+                          Si esta opción se encuentra activada, representa que
+                          el Tipo documental es visible en el sistema y se
+                          podrán realizar operaciones entre cada uno de los
+                          módulos correspondientes de la aplicación. En caso
+                          contrario el Tipo documental no se elimina del sistema
+                          solo quedará inactivo e invisibles para cada uno de
+                          los módulos correspondiente del sistema.
+                        </p>
                       </div>
-                    </Col>
-                  </Row>
-                </CardBody>
-                <CardFooter>
-                  <div className="float-right">
-                    <button type="button" className="btn btn-secondary ">
-                      <i className="fa fa-plus" /> Registrar{" "}
-                    </button>
-                  </div>
-                </CardFooter>
-              </Card>
-            </Col>
-          </Row>
-        </div>
+                    </div>
+                  </Col>
+                </div>
+              </CardBody>
+              <CardFooter>
+                <div className="float-right">
+                  <button
+                    className="btn btn-secondary"
+                    onClick={e => {
+                      this.handleSubmit(e);
+                    }}
+                  >
+                    {" "}
+                    Registrar{" "}
+                  </button>
+                </div>
+              </CardFooter>
+            </Card>
+          </Col>
+        </Row>
       </div>
     );
   }

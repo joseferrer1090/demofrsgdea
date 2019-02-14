c3ba1c3afe852d7f731fa53e37ea25719695fed5
joseferrer1090 <jcfb90@gmail.com>
2019-02-14 16:57:21 -0500

    Change => Adding new checkbox in the view Conglomerado

--- src/views/Conglomerado/components/FormCreateConglomerado.js
+++ src/views/Conglomerado/components/FormCreateConglomerado.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import { Card, CardBody, CardFooter, CardHeader, Button } from "reactstrap";
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Button,
+  CustomInput
+} from "reactstrap";
 
 class FormCreateConglomerado extends Component {
   constructor(props) {
@@ -59,19 +66,21 @@ class FormCreateConglomerado extends Component {
                             {" "}
                             Estado <span className="text-danger">*</span>{" "}
                           </label>
-                          <div className="form-check">
-                            <input
+                          <div className="">
+                            <CustomInput
                               type="checkbox"
-                              className="form-check-input"
-                              id="exampleCheck1"
+                              id="ExampleInputCheckbox"
+                              label=" Si esta opción se encuentra activada, representa
+                              que el conglomerado es visible en el sistema y se
+                              podrán realizar operaciones entre cada uno de los
+                              módulos correspondientes de la aplicación. En caso
+                              contrario el conglomerado no se elimina del
+                              sistema solo quedará inactivo e invisibles para
+                              cada uno de los módulos correspondiente del
+                              sistema."
                             />
-                            <label
-                              className="form-check-label"
-                              htmlFor="exampleCheck1"
-                            >
-                              Activar
-                            </label>
-                            <p
+
+                            {/* <p
                               className="text-muted"
                               style={{ textAlign: "justify" }}
                             >
@@ -83,7 +92,7 @@ class FormCreateConglomerado extends Component {
                               sistema solo quedará inactivo e invisibles para
                               cada uno de los módulos correspondiente del
                               sistema.
-                            </p>
+                            </p> */}
                           </div>
                         </div>
                       </div>

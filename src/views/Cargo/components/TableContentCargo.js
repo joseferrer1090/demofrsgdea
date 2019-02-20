82949d93a0c220cb00051fa54b05f24e14ed052f
joseferrer1090 <jcfb90@gmail.com>
2019-02-20 12:25:42 -0500

    Change => Adding new corrections for tableContentCargo

--- src/views/Cargo/components/TableContentCargo.js
+++ src/views/Cargo/components/TableContentCargo.js
@@ -5,50 +5,63 @@ import ModalView from "./ViewCargoModal";
 import ModalEdit from "./ModalEditCargo";
 import ModalDel from "./ModalDeleteCargo";
 import "./../../../../node_modules/react-bootstrap-table/css/react-bootstrap-table.css";
-import "./../../../css/custom_table.css";
-
+import "./../../../css/table_data.css";
 const dataExample = [
   {
     id: 1,
     codigo: 1233455,
     nombre: "Cargo 1",
     descripcion: "Descripcion del cargo 1",
-    estado: true
+    estado: true,
+    observacion: ""
   },
   {
     id: 2,
     codigo: 23324345,
     nombre: "Cargo 2",
     descripcion: "Descripcion del cargo 2",
-    estado: true
+    estado: true,
+    observacion: ""
   },
   {
     id: 3,
     codigo: 34434545,
     nombre: "Cargo 3",
     descripcion: "Descripcion del cargo 3",
-    estado: true
+    estado: true,
+    observacion: ""
   },
   {
     id: 4,
     codigo: 34567890,
     nombre: "Cargo 3",
     descripcion: "Descripcion del cargo 4",
-    estado: true
+    estado: true,
+    observacion: ""
   },
   {
     id: 5,
     codigo: 9987767,
     nombre: "Cargo 5",
     descripcion: "Descripcion del cargo 5",
-    estado: true
+    estado: true,
+    observacion: ""
   },
   {
     id: 6,
     codigo: 998776,
     nombre: "Cargo 6",
     descripcion: "Descripcion del cargo 6",
-    estado: true
+    estado: true,
+    observacion: ""
+  },
+  {
+    id: 7,
+    codigo: 998776,
+    nombre: "Cargo 7",
+    descripcion: "Descripcion del cargo 7",
+    estado: true,
+    observacion: ""
   }
 ];
 
@@ -62,11 +75,21 @@ class TableContentCargo extends Component {
     };
   }
 
+  CargoStatus = (cell, row) => {
+    let status;
+    if (row.estado === true) {
+      status = <b className="text-success">ACTIVO</b>;
+    } else if (row.estado === false) {
+      status = <b className="text-danger">INACTIVO</b>;
+    }
+    return status;
+  };
+
   accionesCargo(cell, row) {
     return (
       <div
         className="table-menu"
-        style={{ textAlign: "center", padding: "0", marginRight: "65px" }}
+        style={{ textAlign: "center", padding: "0", marginRight: "60px" }}
       >
         <button
           className="btn btn-secondary btn-sm"
@@ -120,14 +143,16 @@ class TableContentCargo extends Component {
       <div className="animated fadeIn">
         <Col md="12">
           <BootstrapTable
-            pagination
-            exportCSV
-            data={dataExample}
+            striped
+            hover
             search
             searchPlaceholder="Buscar"
+            data={dataExample}
+            exportCSV
+            pagination
             bordered={false}
-            hover
-            striped
+            className="texto-small"
+            headerStyle={{ height: "39px" }}
           >
             <TableHeaderColumn
               dataAlign="center"
@@ -135,23 +160,35 @@ class TableContentCargo extends Component {
               isKey
               width={50}
             >
+              #
+            </TableHeaderColumn>
+            <TableHeaderColumn dataAlign="center" dataField="nombre">
+              Nombre
+            </TableHeaderColumn>
+            <TableHeaderColumn
+              dataAlign="center"
+              dataField="codigo"
+              width={100}
+            >
               {" "}
-              #{" "}
+              Código{" "}
             </TableHeaderColumn>
-            <TableHeaderColumn dataAlign="center" dataField={"codigo"}>
-              Código
-            </TableHeaderColumn>
-            <TableHeaderColumn dataAlign="center" dataField={"nombre"}>
-              {" "}
-              Nombre{" "}
-            </TableHeaderColumn>
-            <TableHeaderColumn dataAlign="center" dataField={"descripcion"}>
+            <TableHeaderColumn
+              dataAlign="center"
+              dataField="descripcion"
+              width={500}
+            >
               {" "}
               Descripción{" "}
             </TableHeaderColumn>
-            <TableHeaderColumn dataAlign="center" dataField={"estado"}>
+            <TableHeaderColumn
+              dataAlign="center"
+              dataField="estado"
+              dataFormat={(cell, row) => this.CargoStatus(cell, row)}
+              width="100"
+            >
               {" "}
-              Estado
+              Estado{" "}
             </TableHeaderColumn>
             <TableHeaderColumn
               dataAlign="center"
@@ -162,7 +199,6 @@ class TableContentCargo extends Component {
             </TableHeaderColumn>
           </BootstrapTable>
         </Col>
-
         <ModalView modalviewcargo={this.state.modalview} ref="child1" />
         <ModalEdit modaleditcargo={this.state.modaledit} ref="child2" />
         <ModalDel modaldelete={this.state.modaldelete} ref="child3" />

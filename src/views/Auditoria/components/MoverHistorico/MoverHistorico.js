cb9418d028d837c8fc63f6d5df03519916fddc5c
joseferrer1090 <jcfb90@gmail.com>
2019-02-25 18:11:52 -0500

    Change => starting new view movimiento historico

--- src/views/Auditoria/components/MoverHistorico/MoverHistorico.js
+++ src/views/Auditoria/components/MoverHistorico/MoverHistorico.js
@@ -1,14 +1,48 @@
 import React, { Component } from "react";
+import { TabContent, TabPane, Nav, NavItem, NavLink } from "reactstrap";
+import classnames from "classnames";
 
 class MoverHisotrico extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      activeTab: "1"
+    };
   }
+
+  toggle = tab => {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab
+      });
+    }
+  };
+
   render() {
     return (
-      <div>
-        <p>Probando la ruta</p>
+      <div className="animated fadeIn">
+        <Nav tabs>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === "1" })}
+              onClick={() => {
+                this.toggle("1");
+              }}
+            >
+              <i className="cui-note" /> Manual
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === "2" })}
+              onClick={() => {
+                this.toggle("2");
+              }}
+            >
+              <i className="cui-cog" /> Automatico
+            </NavLink>
+          </NavItem>
+        </Nav>
       </div>
     );
   }

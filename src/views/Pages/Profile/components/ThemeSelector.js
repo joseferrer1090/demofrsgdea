98be552a1f72f1382e4cae7fb4d8f71d74b1e1bd
joseferrer1090 <jcfb90@gmail.com>
2019-02-28 15:09:20 -0500

    Change => base form selector theme

--- src/views/Pages/Profile/components/ThemeSelector.js
+++ src/views/Pages/Profile/components/ThemeSelector.js
@@ -30,182 +30,184 @@ class ThemeSelector extends Component {
               type and scrambled it to make a type specimen book. It has .{" "}
             </Alert>
           </Col>
-          <Col sm="12" height="250">
-            <Card body>
-              <Row>
-                <div className="col-md-12">
-                  <Card body outline color="secondary">
-                    <div className="row">
-                      <div
-                        className="col-md-4"
-                        //   style={{ border: "1px solid red" }}
-                      >
-                        <div className="mt-2">
-                          <input type="radio" className="zoom-radio" />
+          <Col sm="12">
+            <Card>
+              <CardBody style={{ height: "250px !important" }}>
+                <Row>
+                  <div className="col-md-12">
+                    <Card body outline color="secondary">
+                      <div className="row">
+                        <div
+                          className="col-md-4"
+                          //   style={{ border: "1px solid red" }}
+                        >
+                          <div className="mt-2">
+                            <input type="radio" className="zoom-radio" />
+                          </div>
+                        </div>
+                        <div
+                          className="col-md-4"
+                          //   style={{ border: "1px solid red" }}
+                        >
+                          {" "}
+                          <h3 className="text-center">
+                            <a
+                              onClick={() => {
+                                alert("probando");
+                              }}
+                              style={{ cursor: "pointer" }}
+                            >
+                              {" "}
+                              <b> Tema 1 </b>{" "}
+                            </a>
+                          </h3>{" "}
+                        </div>
+                        <div
+                          className="col-md-4"
+                          //   style={{ border: "1px solid red" }}
+                        >
+                          <div className="text-center">
+                            <img
+                              src={themeSVG}
+                              width={30}
+                              className="float-right"
+                            />
+                          </div>
                         </div>
                       </div>
-                      <div
-                        className="col-md-4"
-                        //   style={{ border: "1px solid red" }}
-                      >
-                        {" "}
-                        <h3 className="text-center">
-                          <a
-                            onClick={() => {
-                              alert("probando");
-                            }}
-                            style={{ cursor: "pointer" }}
-                          >
-                            {" "}
-                            <b> Tema 1 </b>{" "}
-                          </a>
-                        </h3>{" "}
-                      </div>
-                      <div
-                        className="col-md-4"
-                        //   style={{ border: "1px solid red" }}
-                      >
-                        <div className="text-center">
-                          <img
-                            src={themeSVG}
-                            width={30}
-                            className="float-right"
-                          />
+                    </Card>
+                  </div>
+                  <div className="col-md-12">
+                    <Card body outline color="secondary">
+                      <div className="row">
+                        <div
+                          className="col-md-4"
+                          //   style={{ border: "1px solid red" }}
+                        >
+                          <div className="mt-2">
+                            <input type="radio" className="zoom-radio" />
+                          </div>
+                        </div>
+                        <div
+                          className="col-md-4"
+                          //   style={{ border: "1px solid red" }}
+                        >
+                          {" "}
+                          <h3 className="text-center">
+                            <a
+                              onClick={() => {
+                                alert("probando");
+                              }}
+                              style={{ cursor: "pointer" }}
+                            >
+                              {" "}
+                              <b> Tema 1 </b>{" "}
+                            </a>
+                          </h3>{" "}
+                        </div>
+                        <div
+                          className="col-md-4"
+                          //   style={{ border: "1px solid red" }}
+                        >
+                          <div className="text-center">
+                            <img
+                              src={themeSVG}
+                              width={30}
+                              className="float-right"
+                            />
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </Card>
-                </div>
-                <div className="col-md-12">
-                  <Card body outline color="secondary">
-                    <div className="row">
-                      <div
-                        className="col-md-4"
-                        //   style={{ border: "1px solid red" }}
-                      >
-                        <div className="mt-2">
-                          <input type="radio" className="zoom-radio" />
+                    </Card>
+                  </div>
+                  <div className="col-md-12">
+                    <Card body outline color="secondary">
+                      <div className="row">
+                        <div
+                          className="col-md-4"
+                          //   style={{ border: "1px solid red" }}
+                        >
+                          <div className="mt-2">
+                            <input type="radio" className="zoom-radio" />
+                          </div>
+                        </div>
+                        <div
+                          className="col-md-4"
+                          //   style={{ border: "1px solid red" }}
+                        >
+                          {" "}
+                          <h3 className="text-center">
+                            <a
+                              onClick={() => {
+                                alert("probando");
+                              }}
+                              style={{ cursor: "pointer" }}
+                            >
+                              {" "}
+                              <b> Tema 1 </b>{" "}
+                            </a>
+                          </h3>{" "}
+                        </div>
+                        <div
+                          className="col-md-4"
+                          //   style={{ border: "1px solid red" }}
+                        >
+                          <div className="text-center">
+                            <img
+                              src={themeSVG}
+                              width={30}
+                              className="float-right"
+                            />
+                          </div>
                         </div>
                       </div>
-                      <div
-                        className="col-md-4"
-                        //   style={{ border: "1px solid red" }}
-                      >
-                        {" "}
-                        <h3 className="text-center">
-                          <a
-                            onClick={() => {
-                              alert("probando");
-                            }}
-                            style={{ cursor: "pointer" }}
-                          >
-                            {" "}
-                            <b> Tema 1 </b>{" "}
-                          </a>
-                        </h3>{" "}
-                      </div>
-                      <div
-                        className="col-md-4"
-                        //   style={{ border: "1px solid red" }}
-                      >
-                        <div className="text-center">
-                          <img
-                            src={themeSVG}
-                            width={30}
-                            className="float-right"
-                          />
+                    </Card>
+                  </div>
+                  <div className="col-md-12">
+                    <Card body outline color="secondary">
+                      <div className="row">
+                        <div
+                          className="col-md-4"
+                          //   style={{ border: "1px solid red" }}
+                        >
+                          <div className="mt-2">
+                            <input type="radio" className="zoom-radio" />
+                          </div>
+                        </div>
+                        <div
+                          className="col-md-4"
+                          //   style={{ border: "1px solid red" }}
+                        >
+                          {" "}
+                          <h3 className="text-center">
+                            <a
+                              onClick={() => {
+                                alert("probando");
+                              }}
+                              style={{ cursor: "pointer" }}
+                            >
+                              {" "}
+                              <b> Tema 1 </b>{" "}
+                            </a>
+                          </h3>{" "}
+                        </div>
+                        <div
+                          className="col-md-4"
+                          //   style={{ border: "1px solid red" }}
+                        >
+                          <div className="text-center">
+                            <img
+                              src={themeSVG}
+                              width={30}
+                              className="float-right"
+                            />
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </Card>
-                </div>
-                <div className="col-md-12">
-                  <Card body outline color="secondary">
-                    <div className="row">
-                      <div
-                        className="col-md-4"
-                        //   style={{ border: "1px solid red" }}
-                      >
-                        <div className="mt-2">
-                          <input type="radio" className="zoom-radio" />
-                        </div>
-                      </div>
-                      <div
-                        className="col-md-4"
-                        //   style={{ border: "1px solid red" }}
-                      >
-                        {" "}
-                        <h3 className="text-center">
-                          <a
-                            onClick={() => {
-                              alert("probando");
-                            }}
-                            style={{ cursor: "pointer" }}
-                          >
-                            {" "}
-                            <b> Tema 1 </b>{" "}
-                          </a>
-                        </h3>{" "}
-                      </div>
-                      <div
-                        className="col-md-4"
-                        //   style={{ border: "1px solid red" }}
-                      >
-                        <div className="text-center">
-                          <img
-                            src={themeSVG}
-                            width={30}
-                            className="float-right"
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  </Card>
-                </div>
-                <div className="col-md-12">
-                  <Card body outline color="secondary">
-                    <div className="row">
-                      <div
-                        className="col-md-4"
-                        //   style={{ border: "1px solid red" }}
-                      >
-                        <div className="mt-2">
-                          <input type="radio" className="zoom-radio" />
-                        </div>
-                      </div>
-                      <div
-                        className="col-md-4"
-                        //   style={{ border: "1px solid red" }}
-                      >
-                        {" "}
-                        <h3 className="text-center">
-                          <a
-                            onClick={() => {
-                              alert("probando");
-                            }}
-                            style={{ cursor: "pointer" }}
-                          >
-                            {" "}
-                            <b> Tema 1 </b>{" "}
-                          </a>
-                        </h3>{" "}
-                      </div>
-                      <div
-                        className="col-md-4"
-                        //   style={{ border: "1px solid red" }}
-                      >
-                        <div className="text-center">
-                          <img
-                            src={themeSVG}
-                            width={30}
-                            className="float-right"
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  </Card>
-                </div>
-              </Row>
+                    </Card>
+                  </div>
+                </Row>
+              </CardBody>
             </Card>
           </Col>
         </Row>

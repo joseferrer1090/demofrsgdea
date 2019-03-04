21b2c1bb5dba49c116ce28f2e9d5785061bb1938
joseferrer1090 <jcfb90@gmail.com>
2019-03-04 11:17:16 -0500

    change => Adding structure for Mensajero

---
+++ src/views/Mensajero/components/ModalActualizarMensajero.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class ModalActualizarMensajero extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="animated fadeIn">
+        <p>Probando</p>
+      </div>
+    );
+  }
+}
+
+ModalActualizarMensajero.propTypes = {};
+
+export default ModalActualizarMensajero;

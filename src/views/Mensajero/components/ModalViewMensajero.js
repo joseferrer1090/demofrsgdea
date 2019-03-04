c630b58b5a6275aad6ba89f57696fefa2d9b50be
joseferrer1090 <jcfb90@gmail.com>
2019-03-04 11:50:01 -0500

    Change => Adding structure for accione in the table mensajero

--- src/views/Mensajero/components/ModalViewMensajero.js
+++ src/views/Mensajero/components/ModalViewMensajero.js
@@ -1,20 +1,43 @@
 import React, { Component } from "react";
+import { Modal, ModalHeader, ModalFooter, ModalBody } from "reactstrap";
 import PropTypes from "prop-types";
 
 class ModalViewMensajero extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      modal: this.props.modalview
+    };
   }
+
+  toggle = () => {
+    this.setState(prevState => ({
+      modal: !prevState.modal
+    }));
+  };
+
   render() {
     return (
-      <div className="animated fadeIn">
-        <p>Probando</p>
+      <div>
+        <Modal>
+          <ModalHeader>Probando</ModalHeader>
+          <ModalBody>
+            <p>Probando</p>
+          </ModalBody>
+          <ModalFooter>
+            <button type="button" className="btn btn-secondary">
+              {" "}
+              <i className="fa fa-times" /> Cerrar{" "}
+            </button>
+          </ModalFooter>
+        </Modal>
       </div>
     );
   }
 }
 
-ModalViewMensajero.propTypes = {};
+ModalViewMensajero.propTypes = {
+  modalview: PropTypes.bool.isRequired
+};
 
 export default ModalViewMensajero;
--- src/views/Mensajero/components/TableContentMensajero.js
+++ src/views/Mensajero/components/TableContentMensajero.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
 import { Row, Col } from "reactstrap";
+import ModalViewMensajero from "./ModalViewMensajero";
 
 const dataExample = [
   {
@@ -42,6 +43,47 @@ const dataExample = [
 ];
 
 class TableContentMensajero extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalView: false
+    };
+  }
+
+  accionesMensajero(cell, row) {
+    return (
+      <div>
+        <button
+          className="btn btn-secondary btn-sm"
+          onClick={() => {
+            alert("probando");
+          }}
+        >
+          {" "}
+          <i className="fa fa-eye" />{" "}
+        </button>
+        &nbsp;
+        <button
+          className="btn btn-secondary btn-sm"
+          onClick={() => {
+            alert("Probando");
+          }}
+        >
+          <i className="fa fa-pencil" />
+        </button>
+        &nbsp;
+        <button
+          className="btn btn-danger btn-sm"
+          onClick={() => {
+            alert("Probando");
+          }}
+        >
+          <i className="fa fa-trash" />
+        </button>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="animated fadeIn">
@@ -81,13 +123,17 @@ class TableContentMensajero extends Component {
                 {" "}
                 Estado{" "}
               </TableHeaderColumn>
-              <TableHeaderColumn dataAlign="center">
+              <TableHeaderColumn
+                dataAlign="center"
+                dataFormat={(cell, row) => this.accionesMensajero(cell, row)}
+              >
                 {" "}
                 Acciones{" "}
               </TableHeaderColumn>
             </BootstrapTable>
           </Col>
         </Row>
+        <ModalViewMensajero modalview={this.state.modalView} ref={"child"} />
       </div>
     );
   }

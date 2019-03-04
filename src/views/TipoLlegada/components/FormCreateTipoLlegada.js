b74e6f0f0aa09f9fea834ea95eef4c998385c3a1
joseferrer1090 <jcfb90@gmail.com>
2019-03-04 17:42:17 -0500

    change => adding structure for tipo de llegafa

---
+++ src/views/TipoLlegada/components/FormCreateTipoLlegada.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import PropType from "prop-types";
+
+class FormCreateTipoLlegada extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="animated fadeIn">
+        <p>Probando</p>
+      </div>
+    );
+  }
+}
+
+FormCreateTipoLlegada.propType = {};
+
+export default FormCreateTipoLlegada;

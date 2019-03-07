942157791967057f43f44ede5697d49372d5775f
joseferrer1090 <jcfb90@gmail.com>
2019-03-07 12:33:06 -0500

    Change => Adding modal view tema

---
+++ src/views/Tema/components/ModalViewTema.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import PropTypes from "prop-types";
+
+class ModalViewTema extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <p>Probando</p>
+      </div>
+    );
+  }
+}
+
+ModalViewTema.propTypes = {};
+
+export default ModalViewTema;

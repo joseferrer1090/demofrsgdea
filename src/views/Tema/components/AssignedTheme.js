dcfb017c6cfa75fd537e1235f816f5cfbed73f5e
joseferrer1090 <jcfb90@gmail.com>
2019-03-07 16:56:38 -0500

    Change => Adding final change for component and view Tema

--- src/views/Tema/components/AssignedTheme.js
+++ src/views/Tema/components/AssignedTheme.js
@@ -1,11 +1,98 @@
 import React, { Component } from "react";
+
 import PropTypes from "prop-types";
 
 class AssignedTheme extends Component {
   render() {
     return (
       <div className="animated fadeIn">
-        <p>Probando</p>
+        <table className="table table-hover table-striped">
+          <thead>
+            <tr className="text-center">
+              <th scope="col">#</th>
+              <th scope="col">Tema</th>
+              <th scope="col">Grupo</th>
+              <th scope="col">Usuarios</th>
+              <th scopen="col"> Activar </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="text-center">
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>
+                <select className="form-control form-control-sm">
+                  {" "}
+                  <option> Seleccione... </option>{" "}
+                </select>
+              </td>
+              <td>
+                <select className="form-control from-control-sm">
+                  {" "}
+                  <option>Seleccione</option>{" "}
+                </select>
+              </td>
+              <td>
+                {" "}
+                <button className="btn btn-secondary btn-sm">
+                  {" "}
+                  <i className="fa fa-power-off" /> Activar{" "}
+                </button>{" "}
+              </td>
+            </tr>
+
+            <tr className="text-center">
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>
+                {" "}
+                <select className="form-control form-control-sm">
+                  {" "}
+                  <option> Seleccione... </option>{" "}
+                </select>
+              </td>
+              <td className="text-center">
+                {" "}
+                <select className="form-control from-control-sm">
+                  {" "}
+                  <option>Seleccione</option>{" "}
+                </select>
+              </td>
+              <td>
+                {" "}
+                <button className="btn btn-secondary btn-sm">
+                  {" "}
+                  <i className="fa fa-power-off" /> Activar{" "}
+                </button>{" "}
+              </td>
+            </tr>
+
+            <tr className="text-center">
+              <th scope="row">3</th>
+              <td>Larry the Bird</td>
+              <td>
+                <select className="form-control form-control-sm">
+                  {" "}
+                  <option> Seleccione... </option>{" "}
+                </select>
+              </td>
+              <td>
+                {" "}
+                <select className="form-control from-control-sm">
+                  {" "}
+                  <option>Seleccione</option>{" "}
+                </select>
+              </td>
+              <td>
+                {" "}
+                <button className="btn btn-secondary btn-sm">
+                  {" "}
+                  <i className="fa fa-power-off" /> Activar{" "}
+                </button>{" "}
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }

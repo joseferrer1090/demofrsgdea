749fffc5e093bcc50ad0b5cc38b9892540e3118b
CristianCuartas <cristianhz1109@gmail.com>
2019-05-15 16:43:05 -0500

    Cambios => Se establecen los elementos correspondientes en la tabla de la sección administrar de sedes

--- src/views/Sedes/components/TableContentSedes.js
+++ src/views/Sedes/components/TableContentSedes.js
@@ -16,7 +16,9 @@ const dataExample = [
     Estado: 0,
     Direccion: "AUTOPISTA SUR  60-51",
     Telefono: "711 90 05",
-    Ciudad: "BOGOTA - DISTRITO CAPITAL"
+    Ciudad: "BOGOTA - DISTRITO CAPITAL",
+    Codigo: 7420,
+    Empresa: "Empresa 1"
   },
   {
     IdSede: "3",
@@ -25,7 +27,9 @@ const dataExample = [
     Estado: 1,
     Direccion: "CALLE 110 9G-330 BODEGA 1415",
     Telefono: "310 398 74 11",
-    Ciudad: "BARRANQUILLA - ATLANTICO"
+    Ciudad: "BARRANQUILLA - ATLANTICO",
+    Codigo: 7430,
+    Empresa: "Empresa 2"
   },
   {
     IdSede: "2",
@@ -34,7 +38,9 @@ const dataExample = [
     Estado: 1,
     Direccion: "CALLE 25D 95A-90",
     Telefono: "426 34 44",
-    Ciudad: "BOGOTA - DISTRITO CAPITAL"
+    Ciudad: "BOGOTA - DISTRITO CAPITAL",
+    Codigo: 7440,
+    Empresa: "Empresa 3"
   },
   {
     IdSede: "6",
@@ -43,7 +49,9 @@ const dataExample = [
     Estado: 0,
     Direccion: "CRA 42 54A-155 BODEGA A 101 ITAGUI",
     Telefono: "4-372 12 45",
-    Ciudad: "MEDELLIN - ANTIOQUIA"
+    Ciudad: "MEDELLIN - ANTIOQUIA",
+    Codigo: 7450,
+    Empresa: "Empresa 4"
   },
   {
     IdSede: "7",
@@ -52,7 +60,9 @@ const dataExample = [
     Estado: 1,
     Direccion: "CRA 14 88-00 BODEGA MONSERRATE BOD 1 SEC. BELMONTE",
     Telefono: "6-320 51 55",
-    Ciudad: "PEREIRA - RISARALDA"
+    Ciudad: "PEREIRA - RISARALDA",
+    Codigo: 7460,
+    Empresa: "Empresa 5"
   },
   {
     IdSede: "5",
@@ -61,7 +71,9 @@ const dataExample = [
     Estado: 0,
     Direccion: "CRA 27B 13-141 ZONA INDUS. BL 8 BOD 15 ARROYOHONDO",
     Telefono: "2-691 37 07",
-    Ciudad: "CALI - VALLE DEL CAUCA"
+    Ciudad: "CALI - VALLE DEL CAUCA",
+    Codigo: 7470,
+    Empresa: "Empresa 6"
   },
   {
     IdSede: "4",
@@ -70,7 +82,9 @@ const dataExample = [
     Estado: 1,
     Direccion: "MANZANA D BODEGA 1 PARQUE INDUSTRIAL VIA CHIMITA",
     Telefono: "7-676 00 78",
-    Ciudad: "BUCARAMANGA - SANTANDER"
+    Ciudad: "BUCARAMANGA - SANTANDER",
+    Codigo: 7410,
+    Empresa: "Empresa 7"
   }
 ];
 
@@ -174,44 +188,29 @@ class TableContentSedes extends Component {
                   #
                 </TableHeaderColumn>
                 <TableHeaderColumn
+                  dataField={"Codigo"}
+                  dataAlign="center"
+                  width={"100"}
+                  dataSort={true}
+                >
+                  Código
+                </TableHeaderColumn>
+                <TableHeaderColumn
+                  dataField={"Empresa"}
+                  dataAlign={"center"}
+                  width={"250"}
+                  dataSort={true}
+                >
+                  {" "}
+                  Empresa{" "}
+                </TableHeaderColumn>
+                <TableHeaderColumn
                   dataField={"Nombre"}
                   dataAlign="center"
                   width={""}
                   dataSort={true}
                 >
                   Nombre
-                </TableHeaderColumn>
-                <TableHeaderColumn
-                  dataField={"Prefijo"}
-                  dataAlign="center"
-                  width={"100"}
-                  dataSort={true}
-                >
-                  Prefijo
-                </TableHeaderColumn>
-                <TableHeaderColumn
-                  dataField={"Ciudad"}
-                  dataAlign={"center"}
-                  width={"250"}
-                  dataSort={true}
-                >
-                  {" "}
-                  Ciudad{" "}
-                </TableHeaderColumn>
-                <TableHeaderColumn
-                  dataField={"Direccion"}
-                  dataAlign={"center"}
-                  width={"250"}
-                  dataSort={true}
-                >
-                  Dirección
-                </TableHeaderColumn>
-                <TableHeaderColumn
-                  dataField={"Telefono"}
-                  dataAlign={"center"}
-                  dataSort={true}
-                >
-                  Teléfono
                 </TableHeaderColumn>
                 <TableHeaderColumn
                   dataField={"Estado"}

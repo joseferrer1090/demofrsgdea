34e2bd46a1f4c2ae96f7e67e1f0f3dad81e72d63
CristianCuartas <cristianhz1109@gmail.com>
2019-05-20 10:24:22 -0500

    Cambios=> Se le asigna su propio css al menú de acciones de la sección administrar de Sedes

--- src/views/Sedes/components/TableContentSedes.js
+++ src/views/Sedes/components/TableContentSedes.js
@@ -109,8 +109,8 @@ class TableContentSedes extends Component {
   accionesSedes = (cell, row) => {
     return (
       <div
-        className="table-menu"
-        style={{ textAlign: "center", padding: "0", marginRight: "30px" }}
+        className="table-actionMenuSedes"
+        style={{ marginRight: "80px" }}
       >
         <button
           className="btn btn-secondary btn-sm"

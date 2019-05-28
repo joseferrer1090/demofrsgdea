6d91c1675706580e46d3026784edd11c30de009b
joseferrer1090 <jcfb90@gmail.com>
2019-05-28 09:02:04 -0500

    Change => Adding TabPane and other component in the route tipo de tercero

--- src/views/TipoTercero/TipoTercero.js
+++ src/views/TipoTercero/TipoTercero.js
@@ -1,11 +1,90 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import {
+  Nav,
+  NavItem,
+  NavLink,
+  TabPane,
+  TabContent,
+  Row,
+  Col
+} from "reactstrap";
+import classnames from "classnames";
 
 class TipoTercero extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: "1"
+    };
+  }
+
+  toggle = tab => {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab
+      });
+    }
+  };
+
   render() {
     return (
-      <div>
-        <p>Probando apenas</p>
+      <div className="animated fadeIn">
+        <Nav tabs>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === "1" })}
+              onClick={() => {
+                this.toggle("1");
+              }}
+            >
+              <i className="fa fa-plus " /> Registrar
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === "2" })}
+              onClick={() => {
+                this.toggle("2");
+              }}
+            >
+              <i className={"fa fa-gear"} /> Administrar
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === "3" })}
+              onClick={() => {
+                this.toggle("3");
+              }}
+            >
+              <i className={"fa fa-upload"} /> Importar
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <TabContent activeTab={this.state.activeTab}>
+          <TabPane tabId="1">
+            <Row>
+              <Col sm="12">
+                <p>Probando apenas </p>¿{" "}
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="2">
+            <Row>
+              <Col sm="12">
+                <p>Probando apenas</p>
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="3">
+            <Row>
+              <Col sm="12">
+                <p>Probando apenas</p>
+              </Col>
+            </Row>
+          </TabPane>
+        </TabContent>
       </div>
     );
   }

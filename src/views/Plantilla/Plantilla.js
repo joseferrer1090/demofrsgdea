0a8f84da2a4bbb506ae718335f2f5f42679652ba
joseferrer1090 <jcfb90@gmail.com>
2019-05-29 10:52:22 -0500

    change => Adding section plantilla de datos

--- src/views/Plantilla/Plantilla.js
+++ src/views/Plantilla/Plantilla.js
@@ -1,20 +1,97 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
+import {
+  TabContent,
+  TabPane,
+  Nav,
+  NavItem,
+  NavLink,
+  Card,
+  Button,
+  CardTitle,
+  CardText,
+  Row,
+  Col
+} from "reactstrap";
+import classnames from "classnames";
+import PropType from "prop-types";
 
 class Plantilla extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      activeTab: "1"
+    };
   }
+
+  toggle = tab => {
+    if (this.state.activeTab !== "tab") {
+      this.setState({
+        activeTab: tab
+      });
+    }
+  };
+
   render() {
     return (
-      <div>
-        <p>Probando apenas section plantilla</p>
+      <div className="animated fadeIn">
+        <Nav tabs>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === "1" })}
+              onClick={() => {
+                this.toggle("1");
+              }}
+            >
+              <i className="fa fa-plus " /> Registrar
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === "2" })}
+              onClick={() => {
+                this.toggle("2");
+              }}
+            >
+              <i className={"fa fa-gear"} /> Administrar
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === "3" })}
+              onClick={() => {
+                this.toggle("3");
+              }}
+            >
+              <i className={"fa fa-upload"} /> Importar
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <TabContent activeTab={this.state.activeTab}>
+          <TabPane tabId="1">
+            <Row>
+              <Col sm="12">
+                <p>Probando registro</p>
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="2">
+            <Row>
+              <Col sm="12">
+                <p>Tabla</p>
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="3">
+            <Row>
+              <Col sm="12">
+                <p>Importe</p>
+              </Col>
+            </Row>
+          </TabPane>
+        </TabContent>
       </div>
     );
   }
 }
-
-Plantilla.propTypes = {};
 
 export default Plantilla;

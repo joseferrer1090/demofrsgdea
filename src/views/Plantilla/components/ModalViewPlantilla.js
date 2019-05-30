bd3171eab9e6692b04581dcdc55e5c666ab09d57
joseferrer1090 <jcfb90@gmail.com>
2019-05-30 17:36:21 -0500

    Change => Adding modal for view plantilla

--- src/views/Plantilla/components/ModalViewPlantilla.js
+++ src/views/Plantilla/components/ModalViewPlantilla.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Col,
+  Row,
+  Card,
+  CardHeader,
+  Collapse,
+  CardBody
+} from "reactstrap";
+import IMGPLANTILLA from "./../../../assets/img/puzzle-pieces.svg";
 
 class ModalViewPlantilla extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      modal: this.props.modalview
+      modal: this.props.modalview,
+      collapse: false
     };
   }
 
@@ -16,14 +29,174 @@ class ModalViewPlantilla extends Component {
     }));
   };
 
+  toggleCollapse = () => {
+    this.setState({
+      collapse: !this.state.collapse
+    });
+  };
+
   render() {
     return (
       <div>
-        <Modal isOpen={this.state.modal}>
+        <Modal className="modal-lg" isOpen={this.state.modal}>
           <ModalHeader>Ver platilla de datos</ModalHeader>
           <ModalBody>
-            <p>Probando apenas</p>
+            <Row>
+              <Col sm="3">
+                <img src={IMGPLANTILLA} className="img-thumbnail" />
+              </Col>
+              <Col sm="9">
+                <div className="">
+                  {" "}
+                  <h5 className="" style={{ borderBottom: "1px solid black" }}>
+                    {" "}
+                    Datos personales{" "}
+                  </h5>{" "}
+                </div>
+
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <dl className="param">
+                        <dt>Código </dt>
+                        <dd> código </dd>
+                      </dl>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <dl className="param">
+                        <dt>Nombre </dt>
+                        <dd> nombre</dd>
+                      </dl>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <dl className="param">
+                        <dt>Descripción </dt>
+                        <dd>descripción</dd>
+                      </dl>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <dl className="param">
+                        <dt>Unidad de correspondencia </dt>
+                        <dd> unidad de correspondencia </dd>
+                      </dl>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <dl className="param">
+                        <dt>Conglomerado </dt>
+                        <dd> conglomerado</dd>
+                      </dl>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <dl className="param">
+                        <dt>Empresa </dt>
+                        <dd> empresa </dd>
+                      </dl>
+                    </div>
+                  </div>
+                </div>
+              </Col>
+              <Col sm="12">
+                <Card>
+                  <CardHeader>
+                    {" "}
+                    <a
+                      onClick={() => {
+                        this.toggleCollapse();
+                      }}
+                      style={{ cursor: "pointer" }}
+                    >
+                      {" "}
+                      Más información{" "}
+                    </a>{" "}
+                  </CardHeader>
+                  <Collapse isOpen={this.state.collapse}>
+                    <CardBody>
+                      <div className="row">
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <dl className="param">
+                              <dt>Sede </dt>
+                              <dd>sede </dd>
+                            </dl>
+                          </div>
+                        </div>
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <dl className="param">
+                              <dt>Dependencias asociadas </dt>
+                              <dd> dependencias asociadas </dd>
+                            </dl>
+                          </div>
+                        </div>
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <dl className="param">
+                              <dt>platilla </dt>
+                              <dd> plantilla </dd>
+                            </dl>
+                          </div>
+                        </div>
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <dl className="param">
+                              <dt>índices de la plantialla </dt>
+                              <dd> índices de la plantilla </dd>
+                            </dl>
+                          </div>
+                        </div>
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <dl className="param">
+                              <dt>Estado </dt>
+                              <dd> estado </dd>
+                            </dl>
+                          </div>
+                        </div>
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <dl className="param">
+                              <dt>Fecha de creación </dt>
+                              <dd> dd/mm/aaaa </dd>
+                            </dl>
+                          </div>
+                        </div>
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <dl className="param">
+                              <dt>Fecha de modificación </dt>
+                              <dd>dd/mm/aaaa </dd>
+                            </dl>
+                          </div>
+                        </div>
+                      </div>
+                    </CardBody>
+                  </Collapse>
+                </Card>
+              </Col>
+            </Row>
           </ModalBody>
+          <ModalFooter>
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={() => {
+                this.setState({ modal: false });
+              }}
+            >
+              {" "}
+              <i className="fa fa-times" /> Cerrar{" "}
+            </button>
+          </ModalFooter>
         </Modal>
       </div>
     );

7027db6689025bf8fabedc95da4b2e1caa014456
joseferrer1090 <jcfb90@gmail.com>
2019-07-04 18:41:18 -0500

    Change => Adding Redux for project

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 

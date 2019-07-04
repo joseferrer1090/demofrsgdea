1a78f7bd41f1de3ce30ebd3f7112886924527a31
joseferrer1090 <jcfb90@gmail.com>
2019-07-04 17:42:25 -0500

    Change => Adding validation for mensajeroForm

--- src/views/Mensajero/components/FormCreateMensajero.js
+++ src/views/Mensajero/components/FormCreateMensajero.js
@@ -1,119 +1,20 @@
-import React, { Component } from "react";
-import PropType from "prop-types";
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Row,
-  Col,
-  CustomInput
-} from "reactstrap";
+import React from "react";
+import MensajeroForm from "./Forms/MensajeroForm";
 
-class FormCreateMensajero extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <div className="animated fadeIn">
-        <Row>
-          <Col sm={{ size: 8, offset: 2 }}>
-            <Card>
-              <CardHeader>Registro de mensajero</CardHeader>
-              <CardBody>
-                <form className="form">
-                  <div className="row">
-                    <div className="col-md-6">
-                      <div className="form-group">
-                        <label>
-                          {" "}
-                          Identificación <span className="text-danger">
-                            *
-                          </span>{" "}
-                        </label>
-                        <input
-                          type="text"
-                          className="form-control form-control-sm"
-                        />
-                      </div>
-                    </div>
-                    <div className="col-md-6">
-                      <div className="form-group">
-                        <label>
-                          {" "}
-                          Nombre <span className="text-danger">*</span>{" "}
-                        </label>
-                        <input
-                          type="text"
-                          className="form-control form-control-sm"
-                        />
-                      </div>
-                    </div>
-                    <div className="col-md-12">
-                      <div className="form-group">
-                        <label> Descripción </label>
-                        <textarea className="form-control form-control-sm" />
-                      </div>
-                    </div>
-                    <div className="col-md-12">
-                      <div className="form-group">
-                        <label>
-                          {" "}
-                          Estado <span className="text-danger">*</span>{" "}
-                        </label>
-                        <div className="text-justify">
-                          <CustomInput
-                            type="checkbox"
-                            id="ExampleCheckboxInput"
-                            label=" Si esta opción se encuentra activada, representa
-                              que el cargo es visible en el sistema y se podrán
-                              realizar operaciones entre cada uno de los módulos
-                              correspondientes de la aplicación. En caso
-                              contrario el cargo no se elimina del sistema solo
-                              quedará inactivo e invisibles para cada uno de los
-                              módulos correspondiente del sistema."
-                          />
-                          {/* <label
-                              className="form-check-label"
-                              htmlFor="exampleCheck1"
-                            >
-                              Activar
-                            </label> */}
-                          {/* <p
-                              className="text-muted"
-                              style={{ textAlign: "justify" }}
-                            >
-                              Si esta opción se encuentra activada, representa
-                              que el cargo es visible en el sistema y se podrán
-                              realizar operaciones entre cada uno de los módulos
-                              correspondientes de la aplicación. En caso
-                              contrario el cargo no se elimina del sistema solo
-                              quedará inactivo e invisibles para cada uno de los
-                              módulos correspondiente del sistema.
-                            </p> */}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </form>
-              </CardBody>
-              <CardFooter>
-                <div className="float-right">
-                  <button className="btn btn-secondary btn-sm">
-                    {" "}
-                    <i className="fa fa-plus" /> Registrar{" "}
-                  </button>
-                </div>
-              </CardFooter>
-            </Card>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
-}
+const dataMensajero = {
+  identificacion: "",
+  nombre: "",
+  descripcion: "",
+  estado: ""
+};
+
+const FormCreateMensajero = () => {
+  return (
+    <div className="animated fadeIn">
+      <MensajeroForm mensajero={dataMensajero} />
+    </div>
+  );
+};
 
 FormCreateMensajero.propType = {};
 

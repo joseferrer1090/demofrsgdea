457ed79a1ae83269f871bb028f9926c18d2fe8a3
joseferrer1090 <jcfb90@gmail.com>
2019-07-04 19:00:07 -0500

    Change => Adding redux on project

--- src/index.js
+++ src/index.js
@@ -8,9 +8,21 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, combineReducers } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { routerMiddleware } from "react-router-redux";
+import createHistory from "./../node_modules/history/createBrowserHistory";
+import reducers from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const history = createHistory();
+const middleware = [routerMiddleware(history), thunk];
+const store = createStore(reducers, applyMiddleware(...middleware));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

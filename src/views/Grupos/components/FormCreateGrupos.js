4041d6d712def4b1af79fece975906b4c2cb2c71
CristianCuartas <cristianhz1109@gmail.com>
2019-07-08 15:45:46 -0500

    Validación Grupo de usuarios

--- src/views/Grupos/components/FormCreateGrupos.js
+++ src/views/Grupos/components/FormCreateGrupos.js
@@ -13,6 +13,7 @@ import {
 import Select from "react-select";
 import AsyncSelect from '../../../../node_modules/react-select/lib/AsyncCreatable';
 import { strict } from "assert";
+import GrupoUsuariosForm from "../Forms/GrupoUsuariosForm";
 
 export const dataGrupoUsuarios=[
   {
@@ -57,6 +58,12 @@ export const dataGrupoUsuarios=[
   }
 ];
 
+const dataGrupoUsers = {
+  codigo: "",
+  nombre: "",
+  descripcion: "",
+};
+
 const filtraritems = [];
 
 class FormCreateGrupos extends Component {
@@ -96,222 +103,7 @@ handleChangeSelectedOptionUsers = selectedOptionUserAsigandos => {
     return (
       <div className="animated fadeIn">
         <div className="container">
-          <Row>
-            <Col sm="8" md={{ offset: 2 }}>
-              <Card>
-                <CardHeader> Registro de grupo de usuarios </CardHeader>
-                <CardBody>
-                  <form>
-                    <div className="row">
-                      <div className="col-md-6">
-                        <div className="form-group">
-                          <label>
-                            {" "}
-                            Código <span className="text-danger">*</span>{" "}
-                          </label>
-                          <input
-                            type="text"
-                            className="form-control form-control-sm"
-                          />
-                        </div>
-                      </div>
-                      <div className="col-md-6">
-                        <div className="form-group">
-                          <label>
-                            {" "}
-                            Nombre <span className="text-danger">*</span>
-                          </label>
-                          <input
-                            type="text"
-                            className="form-control form-control-sm"
-                          />
-                        </div>
-                      </div>
-                      <div className="col-md-12">
-                        <div className="form-group">
-                          <label>
-                            {" "}
-                            Descripción <span className="text-danger">
-                              *{" "}
-                            </span>{" "}
-                          </label>
-                          <textarea className="form-control form-control-sm" />
-                        </div>
-                      </div>
-                    </div>
-                  </form>
-                  <div className="row">
-                    <div className="col-md-12">
-                      <Card>
-                        <CardBody>
-                          <h5 className=""> Búsqueda de usuarios </h5>
-                          <hr />
-                          <br />
-                          <form className="form">
-                            <div className="row">
-                              <div className="col-md-3">
-                                <div className="form-group">
-                                  <label>
-                                    {" "}
-                                    Conglomerado{" "}
-                                    <span className="text-danger">*</span>{" "}
-                                  </label>
-                                  <select className="form-control form-control-sm">
-                                    {" "}
-                                    <option> Seleccione </option>{" "}
-                                  </select>
-                                </div>
-                              </div>
-                              <div className="col-md-3">
-                                <div className="form-group">
-                                  <label>
-                                    {" "}
-                                    Empresa{" "}
-                                    <span className="text-danger">*</span>{" "}
-                                  </label>
-                                  <select className="form-control form-control-sm">
-                                    {" "}
-                                    <option> Seleccione </option>{" "}
-                                  </select>
-                                </div>
-                              </div>
-                              <div className="col-md-3">
-                                <div className="form-group">
-                                  <label>
-                                    {" "}
-                                    Sede <span className="text-danger">
-                                      *
-                                    </span>{" "}
-                                  </label>
-                                  <select className="form-control form-control-sm">
-                                    {" "}
-                                    <option> Seleccione </option>{" "}
-                                  </select>
-                                </div>
-                              </div>
-                              <div className="col-md-3">
-                                <div className="form-group">
-                                  <label>
-                                    {" "}
-                                    Dependencia{" "}
-                                    <span className="text-danger">*</span>{" "}
-                                  </label>
-                                  <select className="form-control form-control-sm">
-                                    {" "}
-                                    <option> Seleccione </option>{" "}
-                                  </select>
-                                </div>
-                              </div>
-                            </div>
-
-                            {dataOk ? (
-                                <div className="form-group">
-                                  <label>Usuarios disponibles</label>
-                                  <select className="form-control form-control-sm"  multiple>
-                                   {buscarOpciones}
-                                  </select>
-                                </div>
-                            ) : null}
-
-                          </form>
-                        </CardBody>
-                        <CardFooter>
-                          <div className="float-right">
-                            <button
-                              type="button"
-                              className="btn btn-secondary btn-sm"
-                              onClick={() => {
-                                this.setState({ dataOk: !this.state.dataOk });
-                              }}
-                            >
-                              {" "}
-                              <i className="fa fa-search" /> Buscar
-                            </button>{" "}
-                          </div>
-                        </CardFooter>
-                      </Card>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-md-12">
-                      <div className="form-group">
-                        <label>
-                          {" "}
-                          Seleccione usuario(s) asignados{" "}
-                          <span className="text-danger">*</span>{" "}
-                        </label>
-                        <Select
-                           onChange={selectedOptionUserAsigandos}
-                           defaultValue={{
-                              value: 'loadUsers',
-                              label: 'Cargar usuarios'
-                            }}
-                            options={filtraritems}
-                            isMulti
-                          />
-                      </div>
-                    </div>
-                  </div>
-                  <br />
-                  <div className="row">
-                    <div className="col-md-12">
-                      <div className="form-group">
-                        <div className="">
-                          <div className="form-group">
-                            <label>
-                              {" "}
-                              Estado <span className="text-danger">*</span>{" "}
-                            </label>
-                            <div className="text-justify">
-                              <CustomInput
-                                type="checkbox"
-                                id="ExampleCheckBoxInput"
-                                label="Si esta opción se encuentra activada, representa
-                                que el grupo es visible en el sistema y se
-                                podrán realizar operaciones entre cada uno de
-                                los módulos correspondientes de la aplicación.
-                                En caso contrario el grupo no se elimina del
-                                sistema solo quedará inactiva e invisibles para
-                                cada uno de los módulos correspondiente del
-                                sistema"
-                              />
-                              {/* <label
-                                className="form-check-label"
-                                htmlFor="exampleCheck1"
-                              >
-                                Activar
-                              </label> */}
-                              {/* <p
-                                className="text-muted"
-                                style={{ textAlign: "justify" }}
-                              >
-                                Si esta opción se encuentra activada, Representa
-                                que el grupo es visible en el sistema y se
-                                podrán realizar operaciones entre cada uno de
-                                los módulos correspondientes de la aplicación.
-                                En caso contrario el grupo no se elimina del
-                                sistema solo quedará inactiva e invisibles para
-                                cada uno de los módulos correspondiente del
-                                sistema
-                              </p> */}
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </CardBody>
-                <CardFooter>
-                  <div className="float-right">
-                    <button type="button" className="btn btn-secondary btn-sm">
-                      {" "}
-                      <i className="fa fa-plus" /> Registrar{" "}
-                    </button>
-                  </div>
-                </CardFooter>
-              </Card>
-            </Col>
-          </Row>
+          <GrupoUsuariosForm grupoUsuarios={dataGrupoUsers}/>
         </div>
       </div>
     );

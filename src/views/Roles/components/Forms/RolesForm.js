dda7088fe6fd52f14f5650372b33b6caf005e2f8
jose carlos <jcfb90@gmail.com>
2019-07-08 23:46:49 -0500

    Change => Starting validation from RolesForm

---
+++ src/views/Roles/components/Forms/RolesForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Formik, withFormik, ErrorMessage } from "formik";
+
+const RolesForm = props => {
+  return (
+    <div>
+      <p>Probando</p>
+    </div>
+  );
+};
+
+export default withFormik({})(RolesForm);

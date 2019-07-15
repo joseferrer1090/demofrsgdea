acc8682f839add2509ca7278639c6e5a34e2d2e2
CristianCuartas <cristianhz1109@gmail>
2019-07-15 15:19:36 -0500

    Cambios => Validación Tipo de terceros

--- src/views/TipoTercero/components/FormCreateTipoTercero.js
+++ src/views/TipoTercero/components/FormCreateTipoTercero.js
@@ -1,118 +1,17 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import {
-  Card,
-  CardBody,
-  CardHeader,
-  CardFooter,
-  Row,
-  Col,
-  CustomInput
-} from "reactstrap";
+import React from "react";
+import TipoTercerosForm from './Form/TipoTercerosForm';
 
-class FormCreateTipoTercero extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <div className="animated fadeIn">
-        <Row>
-          <Col sm={{ size: 8, offset: 2 }}>
-            <Card>
-              <CardHeader> Registrar tipo de tercero </CardHeader>
-              <CardBody>
-                <form className="form">
-                  <div className="row">
-                    <div className="col-md-6">
-                      <div className="form-group">
-                        <label>
-                          {" "}
-                          Código <span className="text-danger"> * </span>
-                        </label>
-                        <input
-                          type="text"
-                          className="form-control form-control-sm"
-                        />
-                      </div>
-                    </div>
-                    <div className="col-md-6">
-                      <div className="form-group">
-                        <label>
-                          {" "}
-                          Nombre <span className="text-danger">*</span>{" "}
-                        </label>
-                        <input
-                          type="text"
-                          className="form-control form-control-sm"
-                        />
-                      </div>
-                    </div>
-                    <div className="col-md-12">
-                      <div className="form-group">
-                        <label>
-                          {" "}
-                          Descripción <span className="text-danger">
-                            *
-                          </span>{" "}
-                        </label>
-                        <textarea className="form-control form-control-sm" />
-                      </div>
-                    </div>
-                    <div className="col-md-12">
-                      <div className="form-group">
-                        <label>
-                          {" "}
-                          Estado <span className="text-danger">*</span>{" "}
-                        </label>
-                        <div className="">
-                          <CustomInput
-                            type="checkbox"
-                            id="ExampleInputCheckbox"
-                            label="Si esta opción se encuentra activada, Representa que
-                             el tipo de tercero es visible en el sistema y se podrán
-                             realizar operaciones entre cada uno de los módulos
-                             correspondientes de la aplicación. En caso contrario
-                             la sede no se elimina del sistema solo quedará
-                             inactiva e invisibles para cada uno de los módulos
-                             correspondiente del sistema."
-                          />
-                        </div>
-                        {/* <p
-                            className="text-muted"
-                            style={{ textAlign: "justify" }}
-                          >
-                            {" "}
-                            Si esta opción se encuentra activada, Representa que
-                            la sede es visible en el sistema y se podrán
-                            realizar operaciones entre cada uno de los módulos
-                            correspondientes de la aplicación. En caso contrario
-                            la sede no se elimina del sistema solo quedará
-                            inactiva e invisibles para cada uno de los módulos
-                            correspondiente del sistema.
-                          </p> */}
-                      </div>
-                    </div>
-                  </div>
-                </form>
-              </CardBody>
-              <CardFooter>
-                <div className="float-right">
-                  <button className="btn btn-secondary btn-sm">
-                    {" "}
-                    <i className="fa fa-plus" /> Registrar{" "}
-                  </button>
-                </div>
-              </CardFooter>
-            </Card>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
+const dataTipoTercerosForm ={
+  codigo:"",
+  nombre:"",
+  estado:""
 }
 
-FormCreateTipoTercero.propTypes = {};
+const  FormCreateTipoTercero =()=>{
+    return (
+      <TipoTercerosForm TipoTercerosForm={dataTipoTercerosForm}/>
+    );
+}
+
 
 export default FormCreateTipoTercero;

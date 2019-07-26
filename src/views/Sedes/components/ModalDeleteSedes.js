84e36b89bca212fbe33d0578071675defc3981bf
joseferrer1090 <jcfb90@gmail.com>
2019-07-26 10:31:48 -0500

    Change => Adding validation in modal delete sedes

--- src/views/Sedes/components/ModalDeleteSedes.js
+++ src/views/Sedes/components/ModalDeleteSedes.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from "reactstrap";
 import PropTypes from "prop-types";
+import * as Yup from "yup";
+import { Formik, ErrorMessage, Field } from "formik";
 
 class ModalDeleteSedes extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modal: this.props.modaldel
-    };
-  }
+  state = {
+    modal: this.props.modaldel,
+    nombre: ""
+  };
 
   toggle = () => {
     this.setState({
@@ -17,47 +17,93 @@ class ModalDeleteSedes extends Component {
   };
 
   render() {
+    const dataPreview = {
+      nombre: ""
+    };
     return (
-      <div>
+      <Fragment>
         <Modal isOpen={this.state.modal}>
           <ModalHeader> Eliminar sede </ModalHeader>
-          <ModalBody>
-            <form className="form">
-              <p className="text-center">
-                {" "}
-                Confirmar el <code> Nombre </code> para eliminar el sede{" "}
-              </p>
+          <Formik
+            initialValues={dataPreview}
+            validationSchema={Yup.object().shape({
+              nombre: Yup.string().required(
+                "Es requirido el nombre para la eliminacion"
+              )
+            })}
+          >
+            {props => {
+              const {
+                values,
+                touched,
+                errors,
+                dirty,
+                isSubmitting,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                handleReset
+              } = props;
+              return (
+                <Fragment>
+                  <ModalBody>
+                    <form className="form">
+                      <p className="text-center">
+                        {" "}
+                        Confirmar el <code> Nombre </code> para eliminar el sede{" "}
+                      </p>
 
-              <input
-                className="form-control col-sm-6 offset-sm-3"
-                type="text"
-                placeholder=""
-                style={{ textAlign: "center" }}
-              />
-              <br />
-              <p className="text-center text-danger">
-                {" "}
-                La sede quedará eliminada de manera permanente.{" "}
-              </p>
-            </form>
-          </ModalBody>
-          <ModalFooter>
-            <Button type="button" className="btn btn-danger">
-              {" "}
-              <i className="fa fa-trash" /> Eliminar{" "}
-            </Button>
-            <Button
-              type="button"
-              className="btn btn-secondary"
-              onClick={() => {
-                this.setState({ modal: false });
-              }}
-            >
-              <i className="fa fa-times" /> Cerrar{" "}
-            </Button>
-          </ModalFooter>
+                      <input
+                        name={"nombre"}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        type="text"
+                        placeholder="nombre para eliminar la sede"
+                        style={{ textAlign: "center" }}
+                        className={`form-control form-control-sm col-sm-6 offset-sm-3 ${errors.nombre &&
+                          touched.nombre &&
+                          "is-invalid"}`}
+                      />
+                      <div className="text-center" style={{ color: "#D54B4B" }}>
+                        {errors.nombre && touched.nombre ? (
+                          <i class="fa fa-exclamation-triangle" />
+                        ) : null}
+                        <ErrorMessage name="nombre" />
+                      </div>
+                      <br />
+                      <p className="text-center text-danger">
+                        {" "}
+                        La sede quedará eliminada de manera permanente.{" "}
+                      </p>
+                    </form>
+                  </ModalBody>
+                  <ModalFooter>
+                    <button
+                      type="button"
+                      className={"btn btn-outline-danger btn-sm"}
+                      onClick={e => {
+                        e.preventDefault();
+                        handleSubmit();
+                      }}
+                    >
+                      <i className="fa fa-trash" /> Eliminar
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-secondary btn-sm"
+                      onClick={() => {
+                        this.setState({ modal: false });
+                      }}
+                    >
+                      <i className="fa fa-times" /> Cerrar{" "}
+                    </button>
+                  </ModalFooter>
+                </Fragment>
+              );
+            }}
+          </Formik>
         </Modal>
-      </div>
+      </Fragment>
     );
   }
 }

48978eda208d54b523ecc21b617811cc04849b40
cristian <cristianhz1109@gmail.com>
2019-09-06 17:23:11 -0500

    Cambios => Se traduce el el tab de registrar de tipo de terceros, en el archivos json ya esta traducida toda la entidad

--- src/views/TipoTercero/components/Forms/TipoTercerosForm.js
+++ src/views/TipoTercero/components/Forms/TipoTercerosForm.js
@@ -14,6 +14,7 @@ import { TYPETHIRDPARTYS } from './../../../../services/EndPoints';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { css } from 'glamor';
+import { withTranslation } from 'react-i18next';
 const TipoTercerosForm = props => {
   const {
     values,
@@ -24,7 +25,8 @@ const TipoTercerosForm = props => {
     handleSubmit,
     isSubmitting,
     setFieldTouched,
-    setFieldValue
+    setFieldValue,
+    t
   } = props;
 
   return (
@@ -33,7 +35,7 @@ const TipoTercerosForm = props => {
         <Col sm={{ size: 8, offset: 2 }}>
           <Card>
             <ToastContainer />
-            <CardHeader> Registrar tipo de tercero </CardHeader>
+            <CardHeader> {t('app_tipoTerecero_tab_title')} </CardHeader>
             <CardBody>
               <form className="form">
                 <div className="row">
@@ -41,7 +43,8 @@ const TipoTercerosForm = props => {
                     <div className="form-group">
                       <label>
                         {' '}
-                        Código <span className="text-danger"> * </span>
+                        {t('app_tipoTerecero_form_registrar_codigo')}{' '}
+                        <span className="text-danger"> * </span>
                       </label>
                       <input
                         name={'code'}
@@ -65,7 +68,8 @@ const TipoTercerosForm = props => {
                     <div className="form-group">
                       <label>
                         {' '}
-                        Nombre <span className="text-danger">*</span>{' '}
+                        {t('app_tipoTerecero_form_registrar_nombre')}{' '}
+                        <span className="text-danger">*</span>{' '}
                       </label>
                       <input
                         name={'name'}
@@ -89,7 +93,8 @@ const TipoTercerosForm = props => {
                     <div className="form-group">
                       <label>
                         {' '}
-                        Descripción <span className="text-danger">*</span>{' '}
+                        {t('app_tipoTerecero_form_registrar_descripcion')}{' '}
+                        <span className="text-danger">*</span>{' '}
                       </label>
                       <textarea
                         name={'description'}
@@ -109,19 +114,16 @@ const TipoTercerosForm = props => {
                     <div className="form-group">
                       <label>
                         {' '}
-                        Estado <span className="text-danger">*</span>{' '}
+                        {t('app_tipoTerecero_form_registrar_estado')}{' '}
+                        <span className="text-danger">*</span>{' '}
                       </label>
                       <div className="">
                         <CustomInput
                           type="checkbox"
                           id="ExampleInputCheckbox"
-                          label="Si esta opción se encuentra activada, Representa que
-                         el tipo de tercero es visible en el sistema y se podrán
-                         realizar operaciones entre cada uno de los módulos
-                         correspondientes de la aplicación. En caso contrario
-                         la sede no se elimina del sistema solo quedará
-                         inactiva e invisibles para cada uno de los módulos
-                         correspondiente del sistema."
+                          label={t(
+                            'app_tipoTerecero_form_registrar_estado_descripcion'
+                          )}
                           name={'status'}
                           onChange={handleChange}
                           onBlur={handleBlur}
@@ -164,7 +166,8 @@ const TipoTercerosForm = props => {
                     <i className=" fa fa-spinner fa-spin" />
                   ) : (
                     <div>
-                      <i className="fa fa-save" /> Guardar
+                      <i className="fa fa-save" />{' '}
+                      {t('app_tipoTerecero_form_registrar_button_guardar')}
                     </div>
                   )}
                 </button>
@@ -177,85 +180,87 @@ const TipoTercerosForm = props => {
   );
 };
 
-export default withFormik({
-  mapPropsToValues: props => ({
-    code: props.TipoTercerosForm.code,
-    name: props.TipoTercerosForm.name,
-    status: props.TipoTercerosForm.status,
-    description: props.TipoTercerosForm.description
-  }),
-  validationSchema: Yup.object().shape({
-    code: Yup.string()
-      .matches(/^[\w]+$/, ' Código no válido.')
-      .min(2, ' Mínimo 2 caracteres')
-      .max(15, ' Máximo 15 caracteres')
-      .required(' Por favor introduzaca un código.'),
-    name: Yup.string()
-      .max(100)
-      .required(' Por favor introduzca un nombre.'),
-    description: Yup.string().max(250, ' Máximo 250 caracteres.'),
-    status: Yup.bool().test(
-      'Activo',
-      'Se requiere la activacion del tipo de tercero.',
-      value => value === true
-    )
-  }),
-  handleSubmit: (values, { setSubmitting, resetForm }) => {
-    const tipoEstado = data => {
-      let tipo = null;
-      if (data === true) {
-        return (tipo = 1);
-      } else if (data === false) {
-        return (tipo = 0);
-      }
-      return null;
-    };
-    setTimeout(() => {
-      fetch(TYPETHIRDPARTYS, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: 'Basic ' + window.btoa('sgdea:123456')
-        },
-        body: JSON.stringify({
-          code: values.code,
-          name: values.name,
-          description: values.description,
-          status: tipoEstado(values.status),
-          userName: 'jferrer'
-        })
-      })
-        .then(response =>
-          response.json().then(data => {
-            if (response.status === 201) {
-              toast.success('Se creo el tipo de tercero con éxito.', {
-                position: toast.POSITION.TOP_RIGHT,
-                className: css({
-                  marginTop: '60px'
-                })
-              });
-              // alert("oki");
-            } else if (response.status === 400) {
-              toast.error('Error, el tipo de tercero ya existe.', {
-                position: toast.POSITION.TOP_RIGHT,
-                className: css({
-                  marginTop: '60px'
-                })
-              });
-              //alert("Erro en el cuerpo");
-            }
+export default withTranslation('translations')(
+  withFormik({
+    mapPropsToValues: props => ({
+      code: props.TipoTercerosForm.code,
+      name: props.TipoTercerosForm.name,
+      status: props.TipoTercerosForm.status,
+      description: props.TipoTercerosForm.description
+    }),
+    validationSchema: Yup.object().shape({
+      code: Yup.string()
+        .matches(/^[\w]+$/, ' Código no válido.')
+        .min(2, ' Mínimo 2 caracteres')
+        .max(15, ' Máximo 15 caracteres')
+        .required(' Por favor introduzaca un código.'),
+      name: Yup.string()
+        .max(100)
+        .required(' Por favor introduzca un nombre.'),
+      description: Yup.string().max(250, ' Máximo 250 caracteres.'),
+      status: Yup.bool().test(
+        'Activo',
+        'Se requiere la activacion del tipo de tercero.',
+        value => value === true
+      )
+    }),
+    handleSubmit: (values, { setSubmitting, resetForm }) => {
+      const tipoEstado = data => {
+        let tipo = null;
+        if (data === true) {
+          return (tipo = 1);
+        } else if (data === false) {
+          return (tipo = 0);
+        }
+        return null;
+      };
+      setTimeout(() => {
+        fetch(TYPETHIRDPARTYS, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Basic ' + window.btoa('sgdea:123456')
+          },
+          body: JSON.stringify({
+            code: values.code,
+            name: values.name,
+            description: values.description,
+            status: tipoEstado(values.status),
+            userName: 'jferrer'
           })
-        )
-        .catch(error => {
-          toast.error(`Error ${error}`, {
-            position: toast.POSITION.TOP_RIGHT,
-            className: css({
-              marginTop: '60px'
+        })
+          .then(response =>
+            response.json().then(data => {
+              if (response.status === 201) {
+                toast.success('Se creo el tipo de tercero con éxito.', {
+                  position: toast.POSITION.TOP_RIGHT,
+                  className: css({
+                    marginTop: '60px'
+                  })
+                });
+                // alert("oki");
+              } else if (response.status === 400) {
+                toast.error('Error, el tipo de tercero ya existe.', {
+                  position: toast.POSITION.TOP_RIGHT,
+                  className: css({
+                    marginTop: '60px'
+                  })
+                });
+                //alert("Erro en el cuerpo");
+              }
             })
+          )
+          .catch(error => {
+            toast.error(`Error ${error}`, {
+              position: toast.POSITION.TOP_RIGHT,
+              className: css({
+                marginTop: '60px'
+              })
+            });
           });
-        });
-      setSubmitting(false);
-      resetForm();
-    }, 1000);
-  }
-})(TipoTercerosForm);
+        setSubmitting(false);
+        resetForm();
+      }, 1000);
+    }
+  })(TipoTercerosForm)
+);

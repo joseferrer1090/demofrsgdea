6802208ae77edc7bb9392c68d76e749775ae87fc
joseferrer1090 <jcfb90@gmail.com>
2019-09-20 11:45:50 -0500

    cambios => empezando la funcionalidad cambio de contraseña

--- src/views/Usuarios/components/FormChangePasswordUser.js
+++ src/views/Usuarios/components/FormChangePasswordUser.js
@@ -1,28 +1,27 @@
 import React, { Component, Fragment } from "react";
-import PropTypes from "prop-types";
 import {
   Modal,
-  ModalFooter,
   ModalHeader,
+  ModalFooter,
   ModalBody,
-  Row,
-  Col,
+  Alert,
   Card,
   CardBody
 } from "reactstrap";
-import { Formik, ErrorMessage, Form } from "formik";
+import PropTypes from "prop-types";
 import * as Yup from "yup";
+import { Formik, withFormik, ErrorMessage, Field, From } from "formik";
 
-class ModalChangePasswordUser extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modal: this.props.modalpassword,
-      id: this.props.id,
-      userLogged: "jferrer",
-      nameUser: ""
-    };
-  }
+class ModalChangePasswordUser extends React.Component {
+  state = {
+    modal: this.props.modalpassword,
+    id: this.props.id,
+    userLogged: "jferrer",
+    nameUser: "",
+    alertSuccess: false,
+    alertError: false,
+    alertCode: false
+  };
 
   toggle = id => {
     this.setState({
@@ -48,25 +47,28 @@ class ModalChangePasswordUser extends Component {
       .catch(Error => console.log(Error));
   };
 
+  onDismiss = () => {
+    this.setState({
+      alertError: false,
+      alertCode: false,
+      alertSuccess: false
+    });
+  };
+
   render() {
+    const dataInitial = {};
+    console.log(this.state.id);
     return (
       <Fragment>
         <Modal isOpen={this.state.modal}>
-          <ModalHeader> Cambiar contraseña {this.state.nameUser} </ModalHeader>
+          <ModalHeader>Eliminar usuario {this.state.nameUser}</ModalHeader>
           <Formik
-            onSubmit={(values, { setSubmitting }) => {
+            onSubmit={(values, setSubmitting) => {
               setTimeout(() => {
                 alert(JSON.stringify(values, "", 2));
-              }, 2000);
+              }, 1000);
             }}
-            validationSchema={Yup.object().shape({
-              newpassword: Yup.string().required(
-                "Se requiere la nueva contraseña"
-              ),
-              confirmpassword: Yup.string().required(
-                "Se requiere confirmar contraseña"
-              )
-            })}
+            validationSchema={Yup.object().shape({})}
           >
             {props => {
               const {
@@ -84,67 +86,97 @@ class ModalChangePasswordUser extends Component {
                 <Fragment>
                   <form className="form">
                     <ModalBody>
-                      <Row>
-                        <Col sm="12">
-                          <p
-                            className="text-muted"
-                            style={{ textAlign: "justify" }}
-                          >
-                            Tener en cuenta que previamente se debe notificar al
-                            usuario
-                            <code> Nombre</code>, que la contraseña se va a
-                            actualizar. En caso contrario se le pueden borrar
-                            las operaciones que se estén realizando en el
-                            sistema
-                          </p>
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col sm="12">
-                          <Card>
-                            <CardBody>
-                              <div className="form-group">
-                                <label>
-                                  {" "}
-                                  Nueva contraseña{" "}
-                                  <span className="text-danger">*</span>{" "}
-                                </label>
-                                <input
-                                  className="form-control form-control-sm"
-                                  type="password"
-                                  placeholder=""
-                                />
-                              </div>
-                              <div className="form-group">
-                                <label>
-                                  {" "}
-                                  Confirmar nueva contraseña{" "}
-                                  <span className="text-danger"> * </span>{" "}
-                                </label>
-                                <input
-                                  type="password"
-                                  className="form-control form-control-sm"
-                                  placeholder=""
-                                />
-                              </div>
-                            </CardBody>
-                          </Card>
-                        </Col>
-                      </Row>
+                      <Alert
+                        color="danger"
+                        isOpen={this.state.alertError}
+                        toggle={this.onDismiss}
+                      >
+                        Error, al eliminar el usuario {values.identificacion}
+                      </Alert>
+                      <Alert
+                        color="success"
+                        isOpen={this.state.alertSuccess}
+                        toggle={this.onDismiss}
+                      >
+                        Se elimino de manera satisfactoria el usuario
+                      </Alert>
+                      <Alert
+                        color="danger"
+                        isOpen={this.state.alertCode}
+                        toggle={this.onDismiss}
+                      >
+                        La identificacion {values.identificacion} para eliminar
+                        no corresponde a usuario
+                      </Alert>
+                      <p
+                        className="text-muted"
+                        style={{ textAlign: "justify" }}
+                      >
+                        Tener en cuenta que previamente se debe notificar al
+                        usuario
+                        <code> Nombre</code>, que la contraseña se va a
+                        actualizar. En caso contrario se le pueden borrar las
+                        operaciones que se estén realizando en el sistema
+                      </p>
+                      <Card>
+                        <CardBody>
+                          <div className="form-group">
+                            <label>
+                              {" "}
+                              Nueva contraseña{" "}
+                              <span className="text-danger">*</span>{" "}
+                            </label>
+                            <input
+                              name={"newpassword"}
+                              value={values.newpassword}
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              className="form-control form-control-sm"
+                              type="password"
+                              placeholder=""
+                            />
+                          </div>
+                          <div className="form-group">
+                            <label>
+                              {" "}
+                              Confirmar contraseña{" "}
+                              <span className="text-danger">*</span>{" "}
+                            </label>
+                            <input
+                              name={"confirmpassword"}
+                              value={values.confirmpassword}
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              className="form-control form-control-sm"
+                              type="password"
+                              placeholder=""
+                            />
+                          </div>
+                        </CardBody>
+                      </Card>
                     </ModalBody>
                     <ModalFooter>
                       <button
                         type="submit"
                         className="btn btn-outline-warning btn-sm"
+                        onClick={e => {
+                          e.preventDefault();
+                          handleSubmit();
+                        }}
                       >
                         {" "}
-                        <i className="fa fa-lock" /> Cambiar contraseña{" "}
+                        <i className="fa fa-trash" /> Eliminar
                       </button>
                       <button
                         type="button"
                         className="btn btn-secondary btn-sm"
                         onClick={() => {
-                          this.setState({ modal: false });
+                          this.setState({
+                            modal: false,
+                            alertError: false,
+                            alertCode: false,
+                            alertSuccess: false
+                          });
                         }}
                       >
                         <i className="fa fa-times" /> Cerrar

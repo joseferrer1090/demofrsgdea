8a74dc84a44548fcf73ff13c329bb84b88a5fa5b
joseferrer1090 <jcfb90@gmail.com>
2019-10-02 16:09:41 -0500

    Cambios se agrego la funcionalidad de cambiar los permisos

--- src/views/Roles/components/ModalEditPermissionRoles.js
+++ src/views/Roles/components/ModalEditPermissionRoles.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment, useState } from "react";
 import PropTypes from "prop-types";
 import Select from "react-select";
 import {
@@ -25,7 +25,11 @@ class ModalEditPermissionRoles extends Component {
     this.state = {
       id: this.props.id,
       modal: this.props.modaleditpermission,
-      dataPermisos: []
+      dataPermisosId: [],
+      dataRolById: {},
+      modulos: [],
+      entidades: [],
+      userName: "jferrer"
     };
   }
 
@@ -34,38 +38,273 @@ class ModalEditPermissionRoles extends Component {
       modal: !this.state.modal,
       id: id
     });
+    this.getDataPermissionById(id);
+    this.getDataRoleById(id);
+  };
+
+  getDataRoleById = id => {
+    fetch(
+      `http://192.168.10.180:7000/api/sgdea/role/${id}?username=${this.state.userName}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Basic " + window.btoa("sgdea:123456")
+        }
+      }
+    )
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          dataRolById: data
+        });
+      })
+      .catch(err => console.log("Err", err));
+  };
+
+  getDataPermissionById = id => {
+    fetch(
+      `http://192.168.10.180:7000/api/sgdea/role/permissions/${id}?username=${this.state.userName}`,
+      {
+        method: "GET",
+        headers: {
+          "Cotent-Type": "application/json",
+          Authorization: "Basic " + window.btoa("sgdea:123456")
+        }
+      }
+    )
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          dataPermisosId: data
+        });
+      })
+      .catch(err => console.log("Error", err));
   };
 
   render() {
+    const dataPreview = this.state.dataPermisosId.map((aux, id) => {
+      return {
+        label: aux.name,
+        value: aux.id
+      };
+    });
     return (
       <Fragment>
-        <Modal className="modal-lg" isOpen={this.state.modal}></Modal>
-        <ModalHeader>Editar Permisos rol</ModalHeader>
-        <Formik>
-          {props => {
-            const {
-              values,
-              touched,
-              errors,
-              dirty,
-              isSubmitting,
-              handleChange,
-              handleBlur,
-              handleSubmit,
-              handleReset
-            } = props;
-            return (
-              <Fragment>
-                <ModalBody>
-                  <p>Hola mundo</p>
-                </ModalBody>
-                <ModalFooter>
-                  <button>Probando</button>
-                </ModalFooter>
-              </Fragment>
-            );
-          }}
-        </Formik>
+        <Modal className="modal-lg" isOpen={this.state.modal}>
+          <ModalHeader>
+            Editar Permisos rol {this.state.dataRolById.name}
+          </ModalHeader>
+          <Formik
+            enableReinitialize={true}
+            initialValues={{
+              permisos: this.state.dataPermisosId.map((aux, id) => {
+                return { label: aux.name, value: aux.id };
+              }),
+              nombre: this.state.dataRolById.name,
+              codigo: this.state.dataRolById.code
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                fetch(`http://192.168.10.180:7000/api/sgdea/role/permissions`, {
+                  method: "PUT",
+                  headers: {
+                    "Content-Type": "application/json",
+                    Authorization: "Basic " + window.btoa("sgdea:123456")
+                  },
+                  body: JSON.stringify({
+                    id: this.state.id,
+                    permissions: values.permisos,
+                    userName: this.state.userName
+                  })
+                })
+                  .then(response => {
+                    if (response === 200) {
+                      console.log("Se hizo el put con los nuevos permisos");
+                    } else if (response.status === 400) {
+                      console.log("Error se enviaron mal los datos");
+                    } else if (response === 500) {
+                      console.log("Error en el Servidor");
+                    }
+                  })
+                  .catch(err => console.log("error"));
+                // alert(JSON.stringify(values, "", 2));
+              }, 1000);
+              setSubmitting(false);
+            }}
+            validationSchema={Yup.object().shape({
+              permisos: Yup.array().of(
+                Yup.object().shape({
+                  label: Yup.string().required(),
+                  value: Yup.string().required()
+                })
+              )
+            })}
+          >
+            {props => {
+              const {
+                values,
+                touched,
+                errors,
+                dirty,
+                isSubmitting,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                handleReset,
+                setFieldTouched,
+                setFieldValue
+              } = props;
+              return (
+                <Fragment>
+                  <ModalBody>
+                    <form>
+                      <Row>
+                        <Col sm="12">
+                          <UncontrolledAlert color="warning">
+                            <div className="text-center">
+                              <i className="fa fa-exclamation-triangle" /> Tenga
+                              en cuenta que cuando se editan los permisos, puede
+                              afectar la sesión de los usuario{" "}
+                              <i className="fa fa-exclamation-triangle" />
+                            </div>
+                          </UncontrolledAlert>
+                        </Col>
+                      </Row>
+                      <br />
+                      <Row>
+                        <Col sm="12">
+                          <Row>
+                            <Col sm="12">
+                              <div className="card card-body">
+                                <div className="row">
+                                  <Col sm="6">
+                                    <div className="form-group">
+                                      <dt>Codigo</dt>
+                                      {/* <dd>{values.codigo}</dd> */}
+                                      <input
+                                        type="text"
+                                        className="form-control form-control-sm"
+                                        disabled
+                                        name="codigo"
+                                        value={values.codigo}
+                                      />
+                                    </div>
+                                  </Col>
+                                  <Col sm="6">
+                                    <div className="form-group">
+                                      <dt>Nombre</dt>
+                                      {/* <dd>{values.nombre}</dd> */}
+                                      <input
+                                        type="text"
+                                        className="form-control form-control-sm"
+                                        disabled
+                                        name="nombre"
+                                        value={values.nombre}
+                                      />
+                                    </div>
+                                  </Col>
+                                </div>
+                              </div>
+                            </Col>
+                            <Col sm="6">
+                              <div className="form-group">
+                                <label>
+                                  {" "}
+                                  Módulo <span className="text-danger">
+                                    *
+                                  </span>{" "}
+                                </label>
+                                <SelectModulo
+                                  name="modulos"
+                                  value={values.modulos}
+                                  onChange={e => {
+                                    setFieldValue("modulos", e.target.value);
+                                  }}
+                                  onBlur={() => {
+                                    setFieldTouched("modulos", true);
+                                  }}
+                                  className={`form-control form-control-sm ${errors.modulos &&
+                                    touched.modulos &&
+                                    "is-invalid"}`}
+                                />
+                              </div>
+                            </Col>
+                            <Col sm="6">
+                              <div className="form-group">
+                                <label>
+                                  {" "}
+                                  Entidades{" "}
+                                  <span className="text-danger">*</span>{" "}
+                                </label>
+                                <MySelectEntidades
+                                  modulo={props.values.modulos}
+                                  name={"entidad"}
+                                  value={values.entidad}
+                                  onChange={e => {
+                                    setFieldValue("entidad", e.target.value);
+                                  }}
+                                  onBlur={() => {
+                                    setFieldTouched("entidad", true);
+                                  }}
+                                  className={`form-control form-control-sm ${errors.entidad &&
+                                    touched.entidad &&
+                                    "is-invalid"}`}
+                                />
+                              </div>
+                            </Col>
+                            {/*  Aqui va la funcionalidad    */}
+                            <Col sm="12">
+                              <div className="form-group">
+                                <label>
+                                  Asignar permisos{" "}
+                                  <span className="text-danger">*</span>{" "}
+                                </label>
+                                <PermisosAsignados
+                                  entidad={props.values.entidad}
+                                  name={"permisos"}
+                                  value={values.permisos}
+                                  onChange={setFieldValue}
+                                  onBlur={setFieldTouched}
+                                  // error={errors.permisos}
+                                  // touched={touched.permisos}
+                                />
+                              </div>
+                            </Col>
+                            {/*  Fin   */}
+                          </Row>
+                        </Col>
+                      </Row>
+                      <Row />
+                    </form>
+                  </ModalBody>
+                  <ModalFooter>
+                    <button
+                      onClick={e => {
+                        e.preventDefault();
+                        handleSubmit();
+                      }}
+                      className="btn btn-outline-warning btn-sm"
+                    >
+                      {" "}
+                      <i className="fa fa-lock" /> Editar permisos{" "}
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-secondary btn-sm"
+                      onClick={() => {
+                        this.setState({ modal: false });
+                      }}
+                    >
+                      {" "}
+                      <i className="fa fa-times" /> Cerrar{" "}
+                    </button>
+                  </ModalFooter>
+                </Fragment>
+              );
+            }}
+          </Formik>
+        </Modal>
       </Fragment>
     );
   }
@@ -77,3 +316,220 @@ ModalEditPermissionRoles.propTypes = {
 };
 
 export default ModalEditPermissionRoles;
+
+class SelectModulo extends React.Component {
+  state = {
+    dataModule: []
+  };
+
+  componentDidMount() {
+    this.getDataModulos();
+  }
+
+  getDataModulos = () => {
+    fetch(`http://192.168.10.180:7000/api/sgdea/module/active`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "Application/json",
+        Authorization: "Basic " + window.btoa("sgdea:123456")
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          dataModule: data
+        });
+      })
+      .catch(err => console.log("", err));
+  };
+
+  handleChange = value => {
+    this.props.onChange("modulos", value);
+  };
+
+  handleBlur = () => {
+    this.props.onBlur("modulos", true);
+  };
+
+  render() {
+    return (
+      <div>
+        <select
+          className="form-control form-control-sm"
+          onChange={this.props.onChange}
+          onBlur={this.props.onBlur}
+          name={this.props.name}
+          value={this.props.value}
+        >
+          <option value={0}> -- seleccione -- </option>
+          {this.state.dataModule.map((aux, id) => {
+            return (
+              <option key={id} value={aux.id}>
+                {aux.name}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    );
+  }
+}
+class MySelectEntidades extends React.Component {
+  state = {
+    dataEntidades: [],
+    id: this.props.modulo
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    if (props.modulo !== state.id) {
+      return {
+        id: props.modulo
+      };
+    }
+    return null;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.modulo !== prevProps.modulo) {
+      this.getDataEntity();
+    }
+  }
+
+  componentDidMount() {
+    this.getDataEntity();
+  }
+
+  getDataEntity = () => {
+    fetch(
+      `http://192.168.10.180:7000/api/sgdea/entity/module/${this.state.id}/active`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Basic " + window.btoa("sgdea:123456")
+        }
+      }
+    )
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          dataEntidades: data
+        });
+        console.log(data);
+      })
+      .catch(err => console.log("Error", err));
+  };
+
+  handleChange = value => {
+    this.props.onChange("entidad", value);
+  };
+
+  handleBlur = () => {
+    this.props.onBlur("entidad", true);
+  };
+
+  render() {
+    // console.log(this.state.id);
+    return (
+      <div>
+        <select
+          name={this.props.name}
+          onChange={this.props.onChange}
+          onBlur={this.props.onBlur}
+          className={this.props.className}
+          value={this.props.value}
+        >
+          <option value={""}>-- seleccione --</option>
+          {this.state.dataEntidades.map((aux, id) => {
+            return (
+              <option key={id} value={aux.id}>
+                {aux.name}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    );
+  }
+}
+
+class PermisosAsignados extends React.Component {
+  state = {
+    dataPermisos: [],
+    id: this.props.entidad
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    if (props.entidad !== state.id) {
+      return {
+        id: props.entidad
+      };
+    }
+    return null;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.entidad !== prevProps.entidad) {
+      this.getPermissionById();
+    }
+  }
+
+  componentDidMount() {
+    this.getPermissionById();
+  }
+
+  getPermissionById = () => {
+    fetch(
+      `http://192.168.10.180:7000/api/sgdea/permission/page/entity/${this.state.id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Basic " + window.btoa("sgdea:123456")
+        }
+      }
+    )
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          dataPermisos: data
+        });
+      })
+      .catch(err => console.log("Error", err));
+  };
+
+  handleChange = value => {
+    this.props.onChange("permisos", value);
+  };
+
+  handleBlur = () => {
+    this.props.onBlur("permisos", true);
+  };
+  render() {
+    console.log(this.state.id);
+    const aux = this.state.dataPermisos.map((aux, id) => {
+      return {
+        id: id,
+        label: aux.name,
+        value: aux.id
+      };
+    });
+    return (
+      <div>
+        <div>
+          <Select
+            name={this.props.name}
+            options={this.state.dataPermisos.map((aux, id) => {
+              return { label: aux.name, value: aux.id };
+            })}
+            isMulti
+            onChange={this.handleChange}
+            onBlur={this.handleBlur}
+            value={this.props.value}
+            placeholder={"Asignar permisos"}
+          />
+        </div>
+      </div>
+    );
+  }
+}

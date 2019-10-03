9eff15349875d7ad237a5cf48532e2db1368f137
jose carlos <jcfb90@gmail.com>
2019-10-03 12:26:04 -0500

    Cambios => resolviendo el problema de la peticion

--- src/views/Grupos/Forms/GrupoUsuariosForm.js
+++ src/views/Grupos/Forms/GrupoUsuariosForm.js
@@ -707,13 +707,16 @@ class MySelect extends React.Component {
   }
 
   getDataUserDependenceList = () => {
-    fetch(`http://192.168.10.180:7000/api/user/dependence/${this.state.id}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Basic " + window.btoa("sgdea:123456")
+    fetch(
+      `http://192.168.10.180:7000/api/user/dependence/${this.props.idDependence}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Basic " + window.btoa("sgdea:123456")
+        }
       }
-    })
+    )
       .then(response => response.json())
       .then(data => {
         this.setState({
@@ -734,6 +737,7 @@ class MySelect extends React.Component {
   };
 
   render() {
+    console.log(this.props.idDependence);
     console.log(this.state.dataUsersDependencia);
     return (
       <div style={{ margin: "0" }}>

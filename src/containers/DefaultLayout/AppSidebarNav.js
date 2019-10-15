7e578bbb281c70fd8fad4319747137936dd42395
cristian <cristianhz1109@gmail.com>
2019-10-15 17:09:21 -0500

    Ajustes en el menu para que se inicialice con todo en ingles

--- src/containers/DefaultLayout/AppSidebarNav.js
+++ src/containers/DefaultLayout/AppSidebarNav.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
-import { Badge, Nav, NavItem, NavLink as RsNavLink } from "reactstrap";
-import classNames from "classnames";
-import PropTypes from "prop-types";
-import PerfectScrollbar from "react-perfect-scrollbar";
-import "react-perfect-scrollbar/dist/css/styles.css";
-import { withTranslation } from "react-i18next";
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Badge, Nav, NavItem, NavLink as RsNavLink } from 'reactstrap';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import { withTranslation } from 'react-i18next';
 
 const propTypes = {
   children: PropTypes.node,
@@ -19,13 +19,13 @@ const propTypes = {
 };
 
 const defaultProps = {
-  tag: "nav",
+  tag: 'nav',
   navConfig: {
     items: [
       {
-        name: "Incio",
-        url: "/configuracion",
-        icon: "icon-home"
+        name: 'Incio',
+        url: '/configuracion',
+        icon: 'icon-home'
       }
     ]
   },
@@ -38,121 +38,121 @@ class AppSidebarNav extends Component {
     this.state = {
       items: [
         {
-          name: `Inicio`,
-          url: "/configuracion",
-          icon: "icon-home"
+          name: `${props.t('nav_app_home')}`,
+          url: '/configuracion',
+          icon: 'icon-home'
         },
         {
-          name: `Conglomerado`,
-          icon: "icon-settings",
-          url: "/configuracion/conglomerado"
+          name: `${props.t('nav_app_conglomerado')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/conglomerado'
         },
         {
-          name: `${props.t("nav_app_empresa")}`,
-          icon: "icon-settings",
-          url: "/configuracion/empresa"
+          name: `${props.t('nav_app_empresa')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/empresa'
         },
         {
-          name: `${props.t("nav_app_sedes")}`,
-          icon: "icon-settings",
-          url: "/configuracion/sedes"
+          name: `${props.t('nav_app_sedes')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/sedes'
         },
         {
-          name: `${props.t("nav_app_dependencia")}`,
-          icon: "icon-settings",
-          url: "/configuracion/dependencia"
+          name: `${props.t('nav_app_dependencia')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/dependencia'
         },
         {
-          name: `${props.t("nav_app_cargo")}`,
-          icon: "icon-settings",
-          url: "/configuracion/cargo"
+          name: `${props.t('nav_app_cargo')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/cargo'
         },
         {
-          name: `${props.t("nav_app_mensajero")}`,
-          icon: "icon-settings ",
-          url: "/configuracion/mensajero"
+          name: `${props.t('nav_app_mensajero')}`,
+          icon: 'icon-settings ',
+          url: '/configuracion/mensajero'
         },
         {
-          name: `${props.t("nav_app_tipo_llegada")}`,
-          icon: "icon-settings",
-          url: "/configuracion/tipollegada"
+          name: `${props.t('nav_app_tipo_llegada')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/tipollegada'
         },
         {
-          name: `${props.t("nav_app_tipo_tramite")}`,
-          icon: "icon-settings",
-          url: "/configuracion/tipotramite"
+          name: `${props.t('nav_app_tipo_tramite')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/tipotramite'
         },
         {
-          name: `${props.t("nav_app_usuarios")}`,
-          icon: "icon-settings",
-          url: "/configuracion/usuarios"
+          name: `${props.t('nav_app_usuarios')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/usuarios'
         },
         {
-          name: `${props.t("nav_app_tipo_terceros")}`,
-          icon: "icon-settings",
-          url: "/configuracion/tipotercero"
+          name: `${props.t('nav_app_tipo_terceros')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/tipotercero'
         },
         {
-          name: `${props.t("nav_app_roles")}`,
-          icon: "icon-settings",
-          url: "/configuracion/roles"
+          name: `${props.t('nav_app_roles')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/roles'
         },
         {
-          name: `${props.t("nav_app_grupo_usuarios")}`,
-          icon: "icon-settings",
-          url: "/configuracion/grupos"
+          name: `${props.t('nav_app_grupo_usuarios')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/grupos'
         },
         {
-          name: `${props.t("nav_app_terceros")}`,
-          icon: "icon-settings",
-          url: "/configuracion/terceros"
+          name: `${props.t('nav_app_terceros')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/terceros'
         },
         {
-          name: `${props.t("nav_app_tipo_radicacion")}`,
-          icon: "icon-book-open",
-          url: "/configuracion/tipodocumentalradicacion"
+          name: `${props.t('nav_app_tipo_radicacion')}`,
+          icon: 'icon-book-open',
+          url: '/configuracion/tipodocumentalradicacion'
         },
         {
-          name: `${props.t("nav_app_pais")}`,
-          icon: "icon-settings",
-          url: "/configuracion/pais"
+          name: `${props.t('nav_app_pais')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/pais'
         },
         {
-          name: `${props.t("nav_app_departamento")}`,
-          icon: "icon-settings",
-          url: "/configuracion/departamento"
+          name: `${props.t('nav_app_departamento')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/departamento'
         },
         {
-          name: `${props.t("nav_app_ciudad")}`,
-          icon: "icon-settings",
-          url: "/configuracion/ciudad"
+          name: `${props.t('nav_app_ciudad')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/ciudad'
         },
         {
-          name: `${props.t("nav_app_auditoria")}`,
-          icon: "icon-info",
-          url: "/configuracion/auditoria",
+          name: `${props.t('nav_app_auditoria')}`,
+          icon: 'icon-info',
+          url: '/configuracion/auditoria',
           children: [
             {
-              name: `${props.t("nav_app_auditoria_consultar")}`,
-              url: "/configuracion/auditoria",
-              icon: "fa fa-search"
+              name: `${props.t('nav_app_auditoria_consultar')}`,
+              url: '/configuracion/auditoria',
+              icon: 'fa fa-search'
             },
             {
-              name: `${props.t("nav_app_auditoria_mover")}`,
-              url: "/configuracion/auditoria/moverhistorico",
-              icon: "fa fa-server"
+              name: `${props.t('nav_app_auditoria_mover')}`,
+              url: '/configuracion/auditoria/moverhistorico',
+              icon: 'fa fa-server'
             }
           ]
         },
         {
-          name: `${props.t("nav_app_plantilla")}`,
-          url: "/configuracion/plantilla",
-          icon: "fa fa-puzzle-piece"
+          name: `${props.t('nav_app_plantilla')}`,
+          url: '/configuracion/plantilla',
+          icon: 'fa fa-puzzle-piece'
         },
         {
-          name: `${props.t("nav_app_tema")}`,
-          icon: "icon-pencil",
-          url: "/configuracion/tema"
+          name: `${props.t('nav_app_tema')}`,
+          icon: 'icon-pencil',
+          url: '/configuracion/tema'
         }
       ],
       t: this.props.t
@@ -181,121 +181,121 @@ class AppSidebarNav extends Component {
     this.setState({
       items: [
         {
-          name: `${t("nav_app_home")}`,
-          url: "/configuracion",
-          icon: "icon-home"
+          name: `${t('nav_app_home')}`,
+          url: '/configuracion',
+          icon: 'icon-home'
         },
         {
-          name: `${t("nav_app_conglomerado")}`,
-          icon: "icon-settings",
-          url: "/configuracion/conglomerado"
+          name: `${t('nav_app_conglomerado')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/conglomerado'
         },
         {
-          name: `${t("nav_app_empresa")}`,
-          icon: "icon-settings",
-          url: "/configuracion/empresa"
+          name: `${t('nav_app_empresa')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/empresa'
         },
         {
-          name: `${t("nav_app_sedes")}`,
-          icon: "icon-settings",
-          url: "/configuracion/sedes"
+          name: `${t('nav_app_sedes')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/sedes'
         },
         {
-          name: `${t("nav_app_dependencia")}`,
-          icon: "icon-settings",
-          url: "/configuracion/dependencia"
+          name: `${t('nav_app_dependencia')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/dependencia'
         },
         {
-          name: `${t("nav_app_cargo")}`,
-          icon: "icon-settings",
-          url: "/configuracion/cargo"
+          name: `${t('nav_app_cargo')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/cargo'
         },
         {
-          name: `${t("nav_app_mensajero")}`,
-          icon: "icon-settings ",
-          url: "/configuracion/mensajero"
+          name: `${t('nav_app_mensajero')}`,
+          icon: 'icon-settings ',
+          url: '/configuracion/mensajero'
         },
         {
-          name: `${t("nav_app_tipo_llegada")}`,
-          icon: "icon-settings",
-          url: "/configuracion/tipollegada"
+          name: `${t('nav_app_tipo_llegada')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/tipollegada'
         },
         {
-          name: `${t("nav_app_tipo_tramite")}`,
-          icon: "icon-settings",
-          url: "/configuracion/tipotramite"
+          name: `${t('nav_app_tipo_tramite')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/tipotramite'
         },
         {
-          name: `${t("nav_app_usuarios")}`,
-          icon: "icon-settings",
-          url: "/configuracion/usuarios"
+          name: `${t('nav_app_usuarios')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/usuarios'
         },
         {
-          name: `${t("nav_app_tipo_terceros")}`,
-          icon: "icon-settings",
-          url: "/configuracion/tipotercero"
+          name: `${t('nav_app_tipo_terceros')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/tipotercero'
         },
         {
-          name: `${t("nav_app_roles")}`,
-          icon: "icon-settings",
-          url: "/configuracion/roles"
+          name: `${t('nav_app_roles')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/roles'
         },
         {
-          name: `${t("nav_app_grupo_usuarios")}`,
-          icon: "icon-settings",
-          url: "/configuracion/grupos"
+          name: `${t('nav_app_grupo_usuarios')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/grupos'
         },
         {
-          name: `${t("nav_app_terceros")}`,
-          icon: "icon-settings",
-          url: "/configuracion/terceros"
+          name: `${t('nav_app_terceros')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/terceros'
         },
         {
-          name: `${t("nav_app_tipo_radicacion")}`,
-          icon: "icon-book-open",
-          url: "/configuracion/tipodocumentalradicacion"
+          name: `${t('nav_app_tipo_radicacion')}`,
+          icon: 'icon-book-open',
+          url: '/configuracion/tipodocumentalradicacion'
         },
         {
-          name: `${t("nav_app_pais")}`,
-          icon: "icon-settings",
-          url: "/configuracion/pais"
+          name: `${t('nav_app_pais')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/pais'
         },
         {
-          name: `${t("nav_app_departamento")}`,
-          icon: "icon-settings",
-          url: "/configuracion/departamento"
+          name: `${t('nav_app_departamento')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/departamento'
         },
         {
-          name: `${t("nav_app_ciudad")}`,
-          icon: "icon-settings",
-          url: "/configuracion/ciudad"
+          name: `${t('nav_app_ciudad')}`,
+          icon: 'icon-settings',
+          url: '/configuracion/ciudad'
         },
         {
-          name: `${t("nav_app_auditoria")}`,
-          icon: "icon-info",
-          url: "/configuracion/auditoria",
+          name: `${t('nav_app_auditoria')}`,
+          icon: 'icon-info',
+          url: '/configuracion/auditoria',
           children: [
             {
-              name: `${t("nav_app_auditoria_consultar")}`,
-              url: "/configuracion/auditoria",
-              icon: "fa fa-search"
+              name: `${t('nav_app_auditoria_consultar')}`,
+              url: '/configuracion/auditoria',
+              icon: 'fa fa-search'
             },
             {
-              name: `${t("nav_app_auditoria_mover")}`,
-              url: "/configuracion/auditoria/moverhistorico",
-              icon: "fa fa-server"
+              name: `${t('nav_app_auditoria_mover')}`,
+              url: '/configuracion/auditoria/moverhistorico',
+              icon: 'fa fa-server'
             }
           ]
         },
         {
-          name: `${t("nav_app_plantilla")}`,
-          url: "/configuracion/plantilla",
-          icon: "fa fa-puzzle-piece"
+          name: `${t('nav_app_plantilla')}`,
+          url: '/configuracion/plantilla',
+          icon: 'fa fa-puzzle-piece'
         },
         {
-          name: `${t("nav_app_tema")}`,
-          icon: "icon-pencil",
-          url: "/configuracion/tema"
+          name: `${t('nav_app_tema')}`,
+          icon: 'icon-pencil',
+          url: '/configuracion/tema'
         }
       ]
     });
@@ -303,18 +303,18 @@ class AppSidebarNav extends Component {
 
   handleClick(e) {
     e.preventDefault();
-    e.currentTarget.parentElement.classList.toggle("open");
+    e.currentTarget.parentElement.classList.toggle('open');
   }
 
   activeRoute(routeName, props) {
     return props.location.pathname.indexOf(routeName) > -1
-      ? "nav-item nav-dropdown open"
-      : "nav-item nav-dropdown";
+      ? 'nav-item nav-dropdown open'
+      : 'nav-item nav-dropdown';
   }
 
   hideMobile() {
-    if (document.body.classList.contains("sidebar-show")) {
-      document.body.classList.toggle("sidebar-show");
+    if (document.body.classList.contains('sidebar-show')) {
+      document.body.classList.toggle('sidebar-show');
     }
   }
 
@@ -338,10 +338,10 @@ class AppSidebarNav extends Component {
 
   // nav list section title
   navTitle(title, key) {
-    const classes = classNames("nav-title", title.class);
+    const classes = classNames('nav-title', title.class);
     return (
       <li key={key} className={classes}>
-        {this.navWrapper(title)}{" "}
+        {this.navWrapper(title)}{' '}
       </li>
     );
   }
@@ -359,20 +359,20 @@ class AppSidebarNav extends Component {
 
   // nav list divider
   navDivider(divider, key) {
-    const classes = classNames("divider", divider.class);
+    const classes = classNames('divider', divider.class);
     return <li key={key} className={classes} />;
   }
 
   // nav label with nav link
   navLabel(item, key) {
     const classes = {
-      item: classNames("hidden-cn", item.class),
-      link: classNames("nav-label", item.class ? item.class : ""),
+      item: classNames('hidden-cn', item.class),
+      link: classNames('nav-label', item.class ? item.class : ''),
       icon: classNames(
-        "nav-icon",
-        !item.icon ? "fa fa-circle" : item.icon,
-        item.label.variant ? `text-${item.label.variant}` : "",
-        item.label.class ? item.label.class : ""
+        'nav-icon',
+        !item.icon ? 'fa fa-circle' : item.icon,
+        item.label.variant ? `text-${item.label.variant}` : '',
+        item.label.class ? item.label.class : ''
       )
     };
     return this.navLink(item, key, classes);
@@ -380,11 +380,11 @@ class AppSidebarNav extends Component {
 
   // nav dropdown
   navDropdown(item, key) {
-    const classIcon = classNames("nav-icon", item.icon);
+    const classIcon = classNames('nav-icon', item.icon);
     const attributes = JSON.parse(JSON.stringify(item.attributes || {}));
     const classes = classNames(
-      "nav-link",
-      "nav-dropdown-toggle",
+      'nav-link',
+      'nav-dropdown-toggle',
       item.class,
       attributes.class
     );
@@ -411,24 +411,24 @@ class AppSidebarNav extends Component {
     const classes = {
       item: classNames(item.class),
       link: classNames(
-        "nav-link",
-        item.variant ? `nav-link-${item.variant}` : ""
+        'nav-link',
+        item.variant ? `nav-link-${item.variant}` : ''
       ),
-      icon: classNames("nav-icon", item.icon)
+      icon: classNames('nav-icon', item.icon)
     };
     return this.navLink(item, key, classes);
   }
 
   // nav link
   navLink(item, key, classes) {
-    const url = item.url || "";
+    const url = item.url || '';
     const itemIcon = <i className={classes.icon} />;
     const itemBadge = this.navBadge(item.badge);
     const attributes = item.attributes || {};
     return (
       <NavItem key={key} className={classes.item}>
         {attributes.disabled ? (
-          <RsNavLink href={""} className={classes.link} {...attributes}>
+          <RsNavLink href={''} className={classes.link} {...attributes}>
             {itemIcon}
             {item.name}
             {itemBadge}
@@ -470,8 +470,8 @@ class AppSidebarNav extends Component {
   }
 
   isExternal(url) {
-    const link = url ? url.substring(0, 4) : "";
-    return link === "http";
+    const link = url ? url.substring(0, 4) : '';
+    return link === 'http';
   }
 
   render() {
@@ -481,11 +481,11 @@ class AppSidebarNav extends Component {
     delete attributes.staticContext;
     delete attributes.Tag;
 
-    const navClasses = classNames(className, "sidebar-nav");
+    const navClasses = classNames(className, 'sidebar-nav');
 
     // ToDo: find better rtl fix
     const isRtl =
-      getComputedStyle(document.documentElement).direction === "rtl";
+      getComputedStyle(document.documentElement).direction === 'rtl';
 
     // sidebar-nav root
     return (
@@ -503,4 +503,4 @@ class AppSidebarNav extends Component {
 AppSidebarNav.propTypes = propTypes;
 AppSidebarNav.defaultProps = defaultProps;
 
-export default withTranslation("translations")(AppSidebarNav);
+export default withTranslation('translations')(AppSidebarNav);

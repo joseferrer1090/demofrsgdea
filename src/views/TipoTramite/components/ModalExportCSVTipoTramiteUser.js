03918898a0dee22a91762eb3b6943314b979bbb0
joseferrer1090 <jcfb90@gmail.com>
2019-10-22 19:43:52 -0500

    Cambios => se agrego en el segundo modal la validacion de la data por el id del tipo de tramite

--- src/views/TipoTramite/components/ModalExportCSVTipoTramiteUser.js
+++ src/views/TipoTramite/components/ModalExportCSVTipoTramiteUser.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment, useState, useEffect } from 'react';
 import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import PropTypes from 'prop-types';
 import { Table } from 'reactstrap';
@@ -14,7 +14,8 @@ class ModalExportCSVTipoTramiteUser extends Component {
       modal: this.props.modalexport2,
       dataExport: [],
       t: this.props.t,
-      username: 'ccuartas'
+      username: 'ccuartas', 
+      tipoTramite: ""
     };
   }
 
@@ -25,6 +26,7 @@ class ModalExportCSVTipoTramiteUser extends Component {
   };
 
   render() {
+      console.log(this.state.tipoTramite);
     return (
       <Fragment>
         <Modal className="modal-xl" isOpen={this.state.modal}>
@@ -32,42 +34,10 @@ class ModalExportCSVTipoTramiteUser extends Component {
            Exportar usuarios por tipo de tramite
           </ModalHeader>
           <ModalBody>
-           <div className="">
-              <div className="form-group row">
-                <label htmlFor="staticEmail" className="col-sm-3 col-form-label" style={{paddingLeft: "70px"}}>Tipo de tramite <span className="text-danger">*</span> :</label>
-                <div className="col-sm-8">
-                     <select className="form-control form-control-sm">
-                         <option>Seleccione</option>
-                     </select>
-                </div>
-            </div>
-           </div>
+             <SelectTipoTramite value={this.state.tipoTramite} onChange={(e) =>{this.setState({ tipoTramite: e.target.value  })}} />
            <div className="row">
                <div className="col-md-12">
-                <table className="table table-responsive table-bordered  table-hover table-striped fixed_header">
-                <thead className="">
-                    <tr className="">
-                    <th>
-                    código
-                    </th>
-                    <th>
-                        nombre
-                    </th>
-                    <th>
-                    descripción
-                    </th>
-                    <th>
-                    respuesta
-                    </th>
-                    <th>
-                    Asunto
-                    </th>     
-                    </tr>
-                </thead>
-              <tbody className="text-justify">
-               
-              </tbody>
-            </table>
+               <TableCSV id={this.state.tipoTramite}/>
                </div>
            </div>
           </ModalBody>
@@ -104,3 +74,124 @@ ModalExportCSVTipoTramiteUser.propTypes = {
 };
 
 export default ModalExportCSVTipoTramiteUser;
+
+const SelectTipoTramite = (props) => {
+    
+    const [data, setData] = useState([]);
+    const [username, setUsername] = useState("jferrer");
+
+
+    useEffect(() => {
+      fetch(`http://192.168.20.187:7000/api/sgdea/typeprocedure/active?username=${username}`, 
+        {
+            method: "GET", 
+            headers:{
+                "Content-Type": "application/json", 
+                Authorization: "Basic " + window.btoa('sgdea:123456')
+            }
+        }).then(response => response.json()).then(data => {
+            setData(data);
+        }).catch(err => console.log("Error", err));
+    }, [username])
+    return(
+          <div className="form-group row">
+                <label htmlFor="staticEmail" className="col-sm-3 col-form-label" style={{paddingLeft: "70px"}}>Tipo de tramite <span className="text-danger">*</span> :</label>
+                <div className="col-sm-8">
+                        <select className="form-control form-control-sm" value={props.value} onChange={props.onChange}>
+                            <option disabled value="">Seleccione tipo de tramite</option>
+                            {
+                                data.map((aux, id) => {
+                                    return(
+                                        <option key={id}  value={aux.id}>{aux.name}</option>
+                                    )
+                                } )
+                            }
+                        </select>
+                </div>
+            </div>
+    )
+}
+
+class TableCSV extends React.Component {
+    state = {
+        data: [], 
+        idTipoTramite: this.props.id, 
+        username: "jferrer"
+    }
+
+    getDataTipoTramiteByUser = () => {
+        fetch(`http://192.168.20.187:7000/api/sgdea/typeprocedure/export/${this.props.id}/users?username=${this.state.username}`, {
+            method: "GET", 
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Basic " + window.btoa('sgdea:123456')
+            }
+        }).then(response => response.json()).then(data => {
+            this.setState({
+                data: data
+            })
+        }).catch(err => console.log("Error", err));
+    }
+
+    static getDerivedStateFromProps(props, state){
+        if(props.id !== state.idTipoTramite ){
+            return{
+                idTipoTramite: props.id
+            };
+        }
+        return null;
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(this.props.id !== prevProps.id){
+            this.getDataTipoTramiteByUser();
+        }
+    }
+
+    componentDidMount() {
+        this.getDataTipoTramiteByUser();
+    }
+    
+
+    render() {
+        console.log(this.state.data);
+        const data = () => {
+            if(Object.keys(this.state.data).length === 0 ){
+                return(
+                     <table className="table table-responsive table-bordered  table-hover table-striped fixed_header">
+                <thead className="">
+                    <tr className="">
+                    <th>
+                    código
+                    </th>
+                    <th>
+                     nombre
+                    </th>
+                    <th>
+                    descripción
+                    </th>
+                    <th>
+                    respuesta
+                    </th>
+                    <th>
+                    Asunto
+                    </th>     
+                    </tr>
+                </thead>
+              <tbody className="text-justify">
+               
+              </tbody>
+            </table>
+                )
+            } else {
+                return <p className="text-center">No hay datos para generar el CSV</p>
+            
+            }
+        }
+        return (
+           <div>
+               {data}
+           </div>
+        );
+    }
+}

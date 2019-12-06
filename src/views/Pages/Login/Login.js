975783a631a3e5b4f5ecd769894d88a5c6e74b40
joseferrer1090 <jcfb90@gmail.com>
2019-12-06 18:32:38 -0500

    Cambios => agregando funcionalidad a la vista de login

--- src/views/Pages/Login/Login.js
+++ src/views/Pages/Login/Login.js
@@ -14,6 +14,8 @@ import {
   InputGroupText,
   Row
 } from "reactstrap";
+import { Formik, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 class Login extends Component {
   constructor(props) {
@@ -24,13 +26,12 @@ class Login extends Component {
     };
   }
 
-  handleChangeInput = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
-
   render() {
+    const validationSchema = Yup.object().shape({
+      username: Yup.string().required("nombre de usuario invalido"),
+      password: Yup.string().required("contraseña es requerida")
+    });
+
     return (
       <div className="app flex-row align-items-center">
         <Container>
@@ -39,73 +40,133 @@ class Login extends Component {
               <CardGroup>
                 <Card className="p-4">
                   <CardBody>
-                    <Form>
-                      <div className="row">
-                        <div
-                          className="col-md-12"
-                          style={{ marginTop: "-42px" }}
-                        >
-                          <div className="text-center">
-                            <img
-                              src="/assets/img/sevenet.svg"
-                              width="150"
-                              height="100"
-                            />
+                    <Formik
+                      validationSchema={validationSchema}
+                      onSubmit={(values, { setSubmitting, resetForm }) => {
+                        setTimeout(() => {
+                          alert(JSON.stringify(values, null, 2));
+                          setSubmitting(false);
+                          resetForm({ values });
+                        }, 1500);
+                      }}
+                    >
+                      {({
+                        values,
+                        errors,
+                        touched,
+                        handleChange,
+                        handleBlur,
+                        handleSubmit,
+                        isSubmitting
+                      }) => (
+                        <Form onSubmit={handleSubmit} noValidate>
+                          <div className="row">
+                            <div
+                              className="col-md-12"
+                              style={{ marginTop: "-42px" }}
+                            >
+                              <div className="text-center">
+                                <img
+                                  src="/assets/img/sevenet.svg"
+                                  width="150"
+                                  height="100"
+                                />
+                              </div>
+                            </div>
                           </div>
-                        </div>
-                      </div>
-                      <h1 className="text-center">Iniciar sesión</h1>
-                      <p className="text-muted text-center">
-                        Ingresa al administrador general SGDEA
-                      </p>
-                      <InputGroup className="mb-3">
-                        <InputGroupAddon addonType="prepend">
-                          <InputGroupText>
-                            <i className="icon-user" />
-                          </InputGroupText>
-                        </InputGroupAddon>
-                        <Input
-                          name="username"
-                          id="username"
-                          type="text"
-                          placeholder="usuario"
-                          autoComplete="usuario"
-                          onChange={e => {
-                            this.handleChangeInput(e);
-                          }}
-                        />
-                      </InputGroup>
-                      <InputGroup className="mb-4">
-                        <InputGroupAddon addonType="prepend">
-                          <InputGroupText>
-                            <i className="icon-lock" />
-                          </InputGroupText>
-                        </InputGroupAddon>
-                        <Input
-                          id="password"
-                          type="password"
-                          placeholder="contraseña"
-                          autoComplete="current-password"
-                          onChange={e => this.handleChangeInput(e)}
-                        />
-                      </InputGroup>
-                      <Row>
-                        <Col xs="6">
-                          <Link
-                            to="/middleware"
-                            className="btn btn-outline-secondary btn-block"
-                          >
-                            Ingresar <i className="fa fa-arrow-circle-right" />
-                            {/* <i className="fa fa-spinner fa-spin" /> */}
-                          </Link>
-                        </Col>
-                        <Col xs="6" className="text-right">
-                          <Link to="/forgot" color="link" className="px-0">
-                            ¿Olvidaste tu contraseña?
-                          </Link>
-                        </Col>
-                      </Row>
-                    </Form>
+                          <h1 className="text-center">Iniciar sesión</h1>
+                          <p className="text-muted text-center">
+                            Ingresa al administrador general SGDEA
+                          </p>
+                          <div className="form-group">
+                            <InputGroup className="mb-3">
+                              <InputGroupAddon addonType="prepend">
+                                <InputGroupText>
+                                  <i className="icon-user" />
+                                </InputGroupText>
+                              </InputGroupAddon>
+                              <input
+                                className="form-control form-control-sm"
+                                name="username"
+                                id="username"
+                                type="text"
+                                placeholder="usuario"
+                                autoComplete="usuario"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                autoFocus
+                              />
+                            </InputGroup>
+                            <div
+                              style={{ color: "#D54B4B", marginTop: "-17px" }}
+                            >
+                              {errors.username && touched.username ? (
+                                <i className="fa fa-exclamation-triangle" />
+                              ) : null}
+                              <ErrorMessage name="username" />
+                            </div>
+                          </div>
+
+                          <div className="form-group">
+                            <InputGroup className="mb-4">
+                              <InputGroupAddon addonType="prepend">
+                                <InputGroupText>
+                                  <i className="icon-lock" />
+                                </InputGroupText>
+                              </InputGroupAddon>
+                              <input
+                                className="form-control form-control-sm"
+                                id="password"
+                                type="password"
+                                placeholder="contraseña"
+                                autoComplete="on"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                              />
+                            </InputGroup>
+                            <div
+                              style={{ color: "#D54B4B", marginTop: "-17px" }}
+                            >
+                              {errors.password && touched.password ? (
+                                <i className="fa fa-exclamation-triangle" />
+                              ) : null}
+                              <ErrorMessage name="password" />
+                            </div>
+                          </div>
+                          <Row>
+                            <Col xs="6">
+                              <button
+                                type="submit"
+                                className="btn btn-outline-secondary btn-block"
+                              >
+                                {/* Ingresar{" "}
+                                <i className="fa fa-arrow-circle-right" /> */}
+                                {isSubmitting ? (
+                                  <i className=" fa fa-spinner fa-spin" />
+                                ) : (
+                                  <div>
+                                    Ingresar{" "}
+                                    <i className="fa fa-arrow-circle-right" />
+                                  </div>
+                                )}
+                              </button>
+                              {/* <Link
+                                to="/middleware"
+                                className="btn btn-outline-secondary btn-block"
+                              >
+                                Ingresar{" "}
+                                <i className="fa fa-arrow-circle-right" />
+                              </Link> */}
+                            </Col>
+                            <Col xs="6" className="text-right">
+                              <Link to="/forgot" color="link" className="px-0">
+                                ¿Olvidaste tu contraseña?
+                              </Link>
+                            </Col>
+                          </Row>
+                        </Form>
+                      )}
+                    </Formik>
                   </CardBody>
                 </Card>
                 {/* <Card

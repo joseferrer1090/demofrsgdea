80446b0ff0c7511f3c120bcc4036880f97a92449
joseferrer1090 <jcfb90@gmail.com>
2019-12-10 17:24:24 -0500

    Cambios => empezando la funcionalidad del login con redux

--- src/services/auth/user.services.js
+++ src/services/auth/user.services.js
@@ -12,6 +12,7 @@ function login(username, password, grant_type) {
   formData.append("username", username);
   formData.append("password", password);
   formData.append("grant_type", grant_type);
+  1;
 
   const requestOptions = {
     method: "POST",

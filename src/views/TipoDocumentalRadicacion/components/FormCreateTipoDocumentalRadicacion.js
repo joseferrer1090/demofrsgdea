7136d57d892076d7dcc2ddf52993bb2854f6bb4e
joseferrer1090 <jcfb90@gmail.com>
2020-01-13 09:56:11 -0500

    Cambios => resolviendo el problema del token

--- src/views/TipoDocumentalRadicacion/components/FormCreateTipoDocumentalRadicacion.js
+++ src/views/TipoDocumentalRadicacion/components/FormCreateTipoDocumentalRadicacion.js
@@ -3,11 +3,10 @@ import TipoDocumentalRadicacion from "./Forms/TipoDocumentalRadicacion";
 import PropTypes from "prop-types";
 
 const FormCreateTipoTramite = props => {
-  console.log(props);
   return (
     <div className="animated fadeIn">
       <div className="row">
-        <TipoDocumentalRadicacion />
+        <TipoDocumentalRadicacion authorization={props.authorization} />
       </div>
     </div>
   );

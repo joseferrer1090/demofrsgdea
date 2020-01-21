57c30b86a4846a08857ed196790b7d0246c320fb
jose carlos <jcfb90@gmail.com>
2020-01-21 14:00:00 -0500

    Cambios => Select de Dependencias listo

---
+++ src/views/TipoDocumentalRadicacion/components/component_viewEdit/SelectDependencia.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { DEPENDENCIES_BY_HEADQUARTER } from "./../../../../services/EndPoints";
+
+class SelectDependencia extends Component {
+  state = {
+    dataDependencia: [],
+    id: this.props.idSede,
+    t: this.props.t,
+    auth: this.props.authorization
+  };
+  static getDerivedStateFromProps(props, state) {
+    if (props.idSede !== state.id) {
+      return {
+        id: props.idSede
+      };
+    }
+    if (props.authorization !== state.auth) {
+      return {
+        auth: props.authorization
+      };
+    }
+    return null;
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.idSede !== prevProps.idSede) {
+      // METODO
+    }
+    if (this.props.authorization !== prevProps.authorization) {
+      this.setState({
+        id: this.props.idSede,
+        auth: this.props.authorization
+      });
+    }
+  }
+  getDataDependencia = () => {
+    fetch(`${DEPENDENCIES_BY_HEADQUARTER}${this.state.id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + this.props.authorization
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          dataDependencia: data
+        });
+      })
+      .catch(err => console.log(`err => ${err}`));
+  };
+
+  render() {
+    const { t } = this.props;
+    return (
+      <div>
+        <select
+          name={this.props.name}
+          value={this.props.value}
+          onChange={this.props.onChange}
+          onBlur={this.props.onBlur}
+          className={this.props.className}
+        >
+          <option>
+            {" "}
+            -- Seleccione --
+            {/* -- {t("app_tipoTramite_form_registrar_select_dependecia")} --{" "} */}
+          </option>
+          {this.state.dataDependencia.map((aux, id) => {
+            return (
+              <option key={id} value={aux.id}>
+                {aux.name}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    );
+  }
+}
+
+SelectDependencia.propTypes = {
+  idSede: PropTypes.string.isRequired,
+  authorization: PropTypes.string.isRequired,
+  t: PropTypes.any
+};
+
+export default SelectDependencia;

65c280dcbce18c112a4217bd1c4c18e2df692a86
jose carlos <jcfb90@gmail.com>
2020-01-21 14:17:11 -0500

    Cambios => estructurando la vista

--- src/views/TipoDocumentalRadicacion/components/ViewEditTipoDocumentalRadication.js
+++ src/views/TipoDocumentalRadicacion/components/ViewEditTipoDocumentalRadication.js
@@ -1,10 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Formik, ErrorMessage, Field } from "formik";
 import * as Yup from "yup";
+import SelectConglomerado from "./component_viewEdit/SelectConglomerado";
+import SelectEmpresa from "./component_viewEdit/SelectEmpresa";
+import SelectSede from "./component_viewEdit/SelectSede";
+import SelectDependencia from "./component_viewEdit/SelectDependencia";
 
 const viewEditTipoDocumental = () => {
-  return <div>Probando apenas</div>;
+  return (
+    <Formik>
+      {props => {
+        const {
+          values,
+          touched,
+          errors,
+          hanbdleChange,
+          handleBlur,
+          handleSubmit,
+          setFieldValue,
+          setFieldTouched
+        } = props;
+        return (
+          <Fragment>
+            <div>Probando</div>
+          </Fragment>
+        );
+      }}
+    </Formik>
+  );
 };
 
 export default viewEditTipoDocumental;

66eb1b962e3d812eeb1c0d34d4e3dba262a7169a
joseferrer1090 <jcfb90@gmail.com>
2020-02-18 12:33:57 -0500

    Cambios => se agrego el changeValue del input de type select

--- src/views/MetaDatos/components/Metadato/components/Types/SelectField.js
+++ src/views/MetaDatos/components/Metadato/components/Types/SelectField.js
@@ -1,11 +1,103 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import * as _ from "lodash";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  TabContent,
+  TabPane,
+  Nav,
+  NavLink,
+  NavItem,
+  Table
+} from "reactstrap";
 
 class SelectField extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      type: "SELECT",
+      toolType: "SELECT_FIELD",
+      title: "",
+      name: "",
+      multiple: false,
+      defaultValue: "",
+      placeholder: "",
+      description: "",
+      validation: {
+        isReadOnly: false,
+        isRequired: false,
+        min: 6,
+        max: 6
+      },
+      options: [],
+      duplicate: false
+    };
   }
+
+  componentDidMount() {
+    this.setState(this.props.field);
+  }
+
+  changeValue = (stateFor, value) => {
+    switch (stateFor) {
+      case "NAME":
+        this.setState({ name: value });
+        break;
+      case "TITLE":
+        this.setState({ title: value });
+        break;
+      case "PLACEHOLDER":
+        this.setState({
+          placeholder: value
+        });
+        break;
+      case "TYPE":
+        this.setState({
+          type: value
+        });
+        break;
+      case "DESCRIPTION":
+        this.setState({
+          description: value
+        });
+        break;
+      case "DEFAULT_VALUE":
+        this.setState({
+          defaultValue: value
+        });
+        break;
+      case "IS_REQUIRED":
+        this.setState({
+          validation: { ...this.state.validation, isRequired: value }
+        });
+        break;
+      case "IS_READONLY":
+        this.setState({
+          validation: { ...this.state.validation, isReadOnly: value }
+        });
+        break;
+      case "MIN":
+        this.setState({ validation: { ...this.state.validation, min: value } });
+        break;
+      case "MAX":
+        this.setState({
+          validation: { ...this.state.validation, max: value }
+        });
+        break;
+      case "DUPLICATE":
+        this.setState({
+          duplicate: value
+        });
+        break;
+
+      default:
+        break;
+    }
+  };
+
   render() {
     return (
       <div>

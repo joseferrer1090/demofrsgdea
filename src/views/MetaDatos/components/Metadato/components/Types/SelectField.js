6f721f4c04552bf23ec5ba47db0e53ed9142101c
joseferrer1090 <jcfb90@gmail.com>
2020-02-18 11:19:07 -0500

    Cambios => Empezando a crear el input de tipo de seleccion

---
+++ src/views/MetaDatos/components/Metadato/components/Types/SelectField.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class SelectField extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <p>Probando</p>
+      </div>
+    );
+  }
+}
+export default SelectField;

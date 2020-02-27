d6e531391515e770a9626e0574af7a52a339c40d
joseferrer1090 <jcfb90@gmail.com>
2020-02-27 14:32:15 -0500

    Cambios => Empezando con el modal de Preview Element

---
+++ src/views/MetaDatos/components/Metadato/components/ModalPreview.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+
+class ModalPreview extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <p>Probando</p>
+      </div>
+    );
+  }
+}
+
+export default ModalPreview;

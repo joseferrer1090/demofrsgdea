7b1b5c879b47fc0ac5de9a0a92d7420be034008c
joseferrer1090 <jcfb90@gmail.com>
2020-03-03 09:57:41 -0500

    Cambios => se tiene que mejorar la seleccion del checked

--- src/views/MetaDatos/components/Metadato/components/ModalPreview.js
+++ src/views/MetaDatos/components/Metadato/components/ModalPreview.js
@@ -119,13 +119,37 @@ class ModalPreview extends Component {
       data.toolType === "CHECK_BOXES"
     ) {
       component = (
-        <div className="form-check">
+        <div
+          className=""
+          style={{ border: "1px solid #c8ced3", padding: "10px" }}
+        >
           <label>{data.title}</label>
           <React.Fragment>
             {data.checkBoxes.length ? (
-              <div>hay valores</div>
+              data.checkBoxes.map((aux, id) => {
+                return (
+                  <div
+                    key={id}
+                    className={data.inline ? "form-check-inline" : "form-check"}
+                  >
+                    <input
+                      name={aux.value}
+                      // checked={aux.selected}
+                      defaultChecked={aux.selected}
+                      className="form-check-input"
+                      type="checkbox"
+                      id={aux.value}
+                    />
+                    <label className="form-check-label" htmlFor={aux.value}>
+                      {aux.title}
+                    </label>
+                  </div>
+                );
+              })
             ) : (
-              <div>no hay valores</div>
+              <div className="form-group">
+                <p className="text-center"> No hay opciones creadas </p>
+              </div>
             )}
           </React.Fragment>
         </div>

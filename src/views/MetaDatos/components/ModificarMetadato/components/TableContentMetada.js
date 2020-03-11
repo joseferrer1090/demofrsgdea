86b61c0870ecacc5e57429ec8059208531daac73
joseferrer1090 <jcfb90@gmail.com>
2020-03-10 19:33:00 -0500

    Cambios => Empezando la funcionalidad de actualizar

---
+++ src/views/MetaDatos/components/ModificarMetadato/components/TableContentMetada.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class TableContentMetada extends Component {
+  render() {
+    return (
+      <div>
+        <p>Probando apenas</p>
+      </div>
+    );
+  }
+}
+
+TableContentMetada.propTypes = {};
+
+export default TableContentMetada;

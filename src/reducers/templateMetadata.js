560dbb96d403450b8f1659e3649682dc1f610098
jose carlos <jcfb90@gmail.com>
2020-03-23 02:50:19 -0500

    Cambios => se agrego la validacion de object en el mismo array relacion plantilla metadato

--- src/reducers/templateMetadata.js
+++ src/reducers/templateMetadata.js
@@ -7,10 +7,15 @@ const initialState = {
 export default function(state = initialState, action) {
   switch (action.type) {
     case AGREGAR_METADATO_PLANTILLA:
-      return {
-        ...state,
-        metadata: [...state.metadata, action.payload]
-      };
+      let index = state.metadata.findIndex(aux => aux.id == action.payload.id);
+      if (index == -1) {
+        return {
+          ...state,
+          metadata: [...state.metadata, action.payload]
+        };
+      } else {
+        return state;
+      }
 
     default:
       return state;

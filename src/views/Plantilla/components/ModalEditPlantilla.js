194c78575fea972b8bcccedf8715d267d6fe1f04
jose carlos <jcfb90@gmail.com>
2020-03-25 18:47:11 -0500

    Cambios => se agrego la base para el modal para editar los valores de la plantilla

---
+++ src/views/Plantilla/components/ModalEditPlantilla.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+
+class ModalEditPlantilla extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <p>probando</p>
+      </div>
+    );
+  }
+}
+
+export default ModalEditPlantilla;

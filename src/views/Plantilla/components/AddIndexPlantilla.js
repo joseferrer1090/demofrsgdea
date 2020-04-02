7eaf753881f37ec0f3b5616bf001f0d0c714a515
jose carlos <jcfb90@gmail.com>
2020-04-02 01:54:33 -0500

    Cambios => se agrego la informacion de la tabla de la asociacion metadato - planitlla

--- src/views/Plantilla/components/AddIndexPlantilla.js
+++ src/views/Plantilla/components/AddIndexPlantilla.js
@@ -85,20 +85,62 @@ class AddIndexPlantilla extends Component {
     return (
       <div className="animated fadeIn">
         {this.state.dataTemplate.length ? (
-          <table className="table table-bordered table-sm table-hover">
-            <thead className="thead-light">
-              <tr className="text-center">
-                <th scope="col">Nombre índice</th>
-                <th scope="col">Tipo</th>
-                <th scope="col">Acciones</th>
-              </tr>
-            </thead>
-            <tbody className="text-center">
-              {this.state.dataTemplate.map((aux, id) => {
-                console.log(aux.metadataBag.name);
-              })}
-            </tbody>
-          </table>
+          <Row>
+            <Col sm={{ size: 10, offset: 1 }}>
+              <Card>
+                <div className="p-2 mb-2 bg-secondary text-black">
+                  <i className="fa fa-puzzle-piece" /> Metadatos asociados en la
+                  plantilla
+                </div>
+                <CardBody>
+                  <div className="btn-toolbar mb-1">
+                    <button
+                      className="btn btn-success btn-sm mr-1"
+                      onClick={() => this.openModalAdd()}
+                    >
+                      <i className="fa fa-plus" /> Nuevo índice
+                    </button>
+                    {/* <button className="btn btn-secondary btn-sm mr-1">
+                      <i className="fa fa-globe" /> Vista previa
+                    </button> */}
+                  </div>
+                  <table className="table table-bordered table-sm table-hover">
+                    <thead className="thead-light">
+                      <tr className="text-center">
+                        <th scope="col">Nombre del metadato</th>
+                        <th scope="col">Tipo</th>
+                        <th scope="col">Acciones</th>
+                      </tr>
+                    </thead>
+                    <tbody className="text-center">
+                      {this.state.dataTemplate.map((aux, id) => {
+                        return (
+                          <tr key={id}>
+                            <td>{aux.metadataBag.name}</td>
+                            <td>{aux.metadataBag.inputType}</td>
+                            <td>
+                              <button
+                                className="btn btn-secondary btn-sm mr-1"
+                                onClick={() => this.openModaEdit()}
+                              >
+                                <i className="fa fa-pencil" />{" "}
+                              </button>
+                              <button
+                                className="btn btn-danger btn-sm mr-1"
+                                onClick={() => this.openModalDelete()}
+                              >
+                                <i className="fa fa-trash" />
+                              </button>
+                            </td>
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </table>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
         ) : (
           <Row>
             <Col sm={{ size: 10, offset: 1 }}>

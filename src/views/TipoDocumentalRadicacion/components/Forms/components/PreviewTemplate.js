c497abac6c885d5ba04d2e19121c88bb52479349
jose carlos <jcfb90@gmail.com>
2020-05-01 13:49:49 -0500

    Cambios => empezando la nueva funcionalidad de registro de la plantilla

---
+++ src/views/TipoDocumentalRadicacion/components/Forms/components/PreviewTemplate.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export const PreviewTemplate = () => {
+  return (
+    <div>
+      <p>Probando apenas</p>
+    </div>
+  );
+};
+
+export default PreviewTemplate;
---
+++ src/views/TipoDocumentalRadicacion/components/Forms/components/SelectPlantilla.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class SelectPlantilla extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <p>probando apenas</p>
+      </div>
+    );
+  }
+}
+
+export default SelectPlantilla;

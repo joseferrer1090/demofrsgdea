ee8b2d7732656d6934d87976d766f10dbd59d947
jose carlos <jcfb90@gmail.com>
2020-06-26 18:08:59 -0500

    cambios => ruta, menu, dela configuracion de estado de correspondencia

---
+++ src/views/EstadoCorrespondencia/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class EstadoCorrespondencia extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <p>probando apenas</p>
+      </div>
+    );
+  }
+}
+
+EstadoCorrespondencia.propTypes = {};
+
+export default EstadoCorrespondencia;
